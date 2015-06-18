98b0a7f2cb38b5cf5f2b317c0634e873f907fc55
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2015-06-18 11:47:13 +0200

    usb_hid: Start other drivers

--- repos/os/src/test/cpu_burner/main.cc
+++ repos/os/src/test/cpu_burner/main.cc
@@ -1,8 +1,17 @@
 #include <base/printf.h>
+#include <base/allocator_avl.h>
+#include <nic_session/connection.h>
+#include <nic/packet_allocator.h>
+
+using namespace Genode;
 
 int main()
 {
 	PINF("Starting CPU burner");
+	try {
+		Nic::Packet_allocator p(env()->heap());
+		Nic::Connection nic(&p, 512 * 1024, 512 * 1024);
+	} catch (...) { }
 	while (1) ;
 
 	return 0;
