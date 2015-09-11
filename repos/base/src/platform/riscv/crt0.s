311cad545fe60eff16afd0d609689bd377599a98
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2015-09-11 13:42:59 +0200

    riscv: Use 'lla' instruction to load intial stack

--- repos/base/src/platform/riscv/crt0.s
+++ repos/base/src/platform/riscv/crt0.s
@@ -3,28 +3,8 @@
 .global _start
 _start:
 
-	/* current ip */
-	auipc sp, 0
-
-	/* lower 12 bits of STACK (negative) */
-	mv  t0, x0
-	addi t0, t0, %lo(STACK)
-
-	/* lower 12 bits of _start */
-	mv  t1, x0
-	addi t1, t1, %lo(_start)
-
-	/* offset form _start to STACK (negative) */
-	sub  t0,  t1, t0
-
-	/* address of STACK */
-	sub t0, sp, t0
-
-	/* read offset of stack_high from STACK */
-	ld t0, (t0)
-
-	/* add to _start */
-	add sp, sp, t0
+	/* load stack pointer */
+	lla sp, _stack_high
 
 	/* relocate linker */
 	jal init_rtld
@@ -40,10 +20,6 @@ _start:
 
 	/* jump into init C-code */
 	j _main
-
-.p2align 3
-	STACK:
-	.quad _stack_high - _start
 
 .bss
 	.p2align 8
