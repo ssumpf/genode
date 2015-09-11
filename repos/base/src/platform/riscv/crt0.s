46d68ab46300ee862ef80e5ea028923a03c6dfaa
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2015-09-11 10:15:08 +0200

    riscv: crt0.s add ld.lib.so compatibility

--- repos/base/src/platform/riscv/crt0.s
+++ repos/base/src/platform/riscv/crt0.s
@@ -3,7 +3,31 @@
 .global _start
 _start:
 
-	la sp, _stack_high
+	/* current ip */
+	auipc sp, 0
+
+	/* lower 12 bits of STACK (negative) */
+	mv  t0, x0
+	addi t0, t0, %lo(STACK)
+
+	/* lower 12 bits of _start */
+	mv  t1, x0
+	addi t1, t1, %lo(_start)
+
+	/* offset form _start to STACK (negative) */
+	sub  t0,  t1, t0
+
+	/* address of STACK */
+	sub t0, sp, t0
+
+	/* read offset of stack_high from STACK */
+	ld t0, (t0)
+
+	/* add to _start */
+	add sp, sp, t0
+
+	/* relocate linker */
+	jal init_rtld
 
 	/* create environment for main thread */
 	jal init_main_thread
@@ -17,7 +41,11 @@ _start:
 	/* jump into init C-code */
 	j _main
 
+.p2align 3
+	STACK:
+	.quad _stack_high - _start
+
 .bss
-	.p2align 16
+	.p2align 8
 	.space 32*1024
 	_stack_high:
