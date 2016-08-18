7a8739c6ae271c6a9c9ab42ab6c47a0153d79872
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2016-08-18 12:20:25 +0200

    mesa-11: use Genode's blit library for pixel transfer

--- repos/libports/src/lib/mesa/egl/genode_interface.cc
+++ repos/libports/src/lib/mesa/egl/genode_interface.cc
@@ -1,5 +1,6 @@
 /* needed since there exists a 'util/list.h' in mesa */
 #include <../include/util/list.h>
+#include <blit/blit.h>
 #include <window.h>
 
 extern "C" {
@@ -11,6 +12,12 @@ void genode_framebuffer_refresh(Genode_egl_window *win, int x, int y, int w, int
 {
 	Window *window = static_cast<Window *>(win);
 	window->framebuffer->refresh(x, y, w, h);
+}
+
+
+void genode_blit(void const *src, unsigned src_w, void *dst, unsigned dst_w, int w, int h)
+{
+	blit(src, src_w, dst, dst_w, w, h);
 }
 
 
