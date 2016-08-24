e3858729214406053ff6f7bff06add47b842c771
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2016-08-24 12:21:49 +0200

    mesa-11: start i965 initialization
    + add 'genode_ioctl' binding to drm

---
+++ repos/libports/src/lib/drm/ioctl.cc
@@ -0,0 +1,84 @@
+extern "C" {
+#include <drm.h>
+#include <i915_drm.h>
+}
+
+#include <base/printf.h>
+
+enum { verbose_ioctl = true };
+
+
+long drm_command(long request) { return (request & 0xff) - DRM_COMMAND_BASE; }
+
+
+/**
+ * Return name of DRM command
+ */
+const char *command_name(long request)
+{
+	if (IOCGROUP(request) != DRM_IOCTL_BASE)
+		return "<non-DRM>";
+
+	switch (drm_command(request)) {
+	case DRM_I915_INIT:                  return "DRM_I915_INIT";
+	case DRM_I915_FLUSH:                 return "DRM_I915_FLUSH";
+	case DRM_I915_FLIP:                  return "DRM_I915_FLIP";
+	case DRM_I915_BATCHBUFFER:           return "DRM_I915_BATCHBUFFER";
+	case DRM_I915_IRQ_EMIT:              return "DRM_I915_IRQ_EMIT";
+	case DRM_I915_IRQ_WAIT:              return "DRM_I915_IRQ_WAIT";
+	case DRM_I915_GETPARAM:              return "DRM_I915_GETPARAM";
+	case DRM_I915_SETPARAM:              return "DRM_I915_SETPARAM";
+	case DRM_I915_ALLOC:                 return "DRM_I915_ALLOC";
+	case DRM_I915_FREE:                  return "DRM_I915_FREE";
+	case DRM_I915_INIT_HEAP:             return "DRM_I915_INIT_HEAP";
+	case DRM_I915_CMDBUFFER:             return "DRM_I915_CMDBUFFER";
+	case DRM_I915_DESTROY_HEAP:          return "DRM_I915_DESTROY_HEAP";
+	case DRM_I915_SET_VBLANK_PIPE:       return "DRM_I915_SET_VBLANK_PIPE";
+	case DRM_I915_GET_VBLANK_PIPE:       return "DRM_I915_GET_VBLANK_PIPE";
+	case DRM_I915_VBLANK_SWAP:           return "DRM_I915_VBLANK_SWAP";
+	case DRM_I915_HWS_ADDR:              return "DRM_I915_HWS_ADDR";
+	case DRM_I915_GEM_INIT:              return "DRM_I915_GEM_INIT";
+	case DRM_I915_GEM_EXECBUFFER:        return "DRM_I915_GEM_EXECBUFFER";
+	case DRM_I915_GEM_PIN:               return "DRM_I915_GEM_PIN";
+	case DRM_I915_GEM_UNPIN:             return "DRM_I915_GEM_UNPIN";
+	case DRM_I915_GEM_BUSY:              return "DRM_I915_GEM_BUSY";
+	case DRM_I915_GEM_THROTTLE:          return "DRM_I915_GEM_THROTTLE";
+	case DRM_I915_GEM_ENTERVT:           return "DRM_I915_GEM_ENTERVT";
+	case DRM_I915_GEM_LEAVEVT:           return "DRM_I915_GEM_LEAVEVT";
+	case DRM_I915_GEM_CREATE:            return "DRM_I915_GEM_CREATE";
+	case DRM_I915_GEM_PREAD:             return "DRM_I915_GEM_PREAD";
+	case DRM_I915_GEM_PWRITE:            return "DRM_I915_GEM_PWRITE";
+	case DRM_I915_GEM_MMAP:              return "DRM_I915_GEM_MMAP";
+	case DRM_I915_GEM_SET_DOMAIN:        return "DRM_I915_GEM_SET_DOMAIN";
+	case DRM_I915_GEM_SW_FINISH:         return "DRM_I915_GEM_SW_FINISH";
+	case DRM_I915_GEM_SET_TILING:        return "DRM_I915_GEM_SET_TILING";
+	case DRM_I915_GEM_GET_TILING:        return "DRM_I915_GEM_GET_TILING";
+	case DRM_I915_GEM_GET_APERTURE:      return "DRM_I915_GEM_GET_APERTURE";
+	case DRM_I915_GEM_MMAP_GTT:          return "DRM_I915_GEM_MMAP_GTT";
+	case DRM_I915_GET_PIPE_FROM_CRTC_ID: return "DRM_I915_GET_PIPE_FROM_CRTC_ID";
+	case DRM_I915_GEM_MADVISE:           return "DRM_I915_GEM_MADVISE";
+	case DRM_I915_OVERLAY_PUT_IMAGE:     return "DRM_I915_OVERLAY_PUT_IMAGE";
+	case DRM_I915_OVERLAY_ATTRS:         return "DRM_I915_OVERLAY_ATTRS";
+	case DRM_I915_GEM_EXECBUFFER2:       return "DRM_I915_GEM_EXECBUFFER2";
+	default:                             return "<unknown>";
+	}
+}
+
+
+static void dump_ioctl(long request)
+{
+	PDBG("ioctl(request=%lx, %s, len=%ld, cmd=%s)", request,
+	     (request & 0xe0000000) == IOC_OUT   ? "out"   :
+	     (request & 0xe0000000) == IOC_IN    ? "in"    :
+	     (request & 0xe0000000) == IOC_INOUT ? "inout" : "void",
+	     IOCPARM_LEN(request),
+	     command_name(request));
+}
+
+extern "C" int genode_ioctl(int fd, unsigned long request, void *arg)
+{
+	if (verbose_ioctl)
+		dump_ioctl(request);
+
+	return 0;
+}
