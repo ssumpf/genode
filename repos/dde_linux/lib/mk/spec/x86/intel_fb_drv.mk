182e7861d649783ff7c4b132ce503fe04b829b55
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2016-08-24 14:28:38 +0200

    intel_fb_drv: move everything put startup code to lib

--- repos/dde_linux/lib/mk/spec/x86/intel_fb_drv.mk
+++ repos/dde_linux/lib/mk/spec/x86/intel_fb_drv.mk
@@ -1,7 +1,7 @@
-LX_CONTRIB_DIR := $(call select_from_ports,dde_linux)/src/drivers/framebuffer/intel
-SRC_DIR        := $(REP_DIR)/src/drivers/framebuffer/intel
+LX_CONTRIB_DIR := $(call select_from_ports,dde_linux)/src/lib/framebuffer/intel
+SRC_DIR         = $(REP_DIR)/src/lib/framebuffer/intel
 
-LIBS    += intel_fb_include
+LIBS    += intel_fb_include libc-setjmp blit
 
 SRC_C   :=
 SRC_C   += $(notdir $(wildcard $(LX_CONTRIB_DIR)/drivers/char/agp/*.c))
@@ -14,12 +14,28 @@ SRC_C   += $(notdir $(wildcard $(LX_CONTRIB_DIR)/drivers/video/fbdev/core/*.c))
 SRC_C   += $(notdir $(wildcard $(LX_CONTRIB_DIR)/lib/*.c))
 
 #
+# Genode part
+#
+SRC_C    += dummies.c i915_params.c lx_emul_c.c
+SRC_CC   += main.cc lx_emul.cc
+
+# lx_kit
+SRC_CC  += irq.cc \
+           malloc.cc \
+           mapped_io_mem_range.cc \
+           pci.cc \
+           printf.cc \
+           scheduler.cc \
+           timer.cc \
+           work.cc
+
+#
 # Reduce build noise of compiling contrib code
 #
-CC_WARN = -Wall -Wno-uninitialized -Wno-unused-but-set-variable \
-          -Wno-unused-variable -Wno-unused-function \
-          -Wno-pointer-arith -Wno-pointer-sign \
-          -Wno-int-to-pointer-cast
+CC_C_OPT += -Wall -Wno-uninitialized -Wno-unused-but-set-variable \
+            -Wno-unused-variable -Wno-unused-function \
+            -Wno-pointer-arith -Wno-pointer-sign \
+            -Wno-int-to-pointer-cast -Wno-maybe-uninitialized
 
 vpath %.c $(LX_CONTRIB_DIR)/drivers/char/agp
 vpath %.c $(LX_CONTRIB_DIR)/drivers/i2c
@@ -29,4 +45,8 @@ vpath %.c $(LX_CONTRIB_DIR)/drivers/gpu/drm
 vpath %.c $(LX_CONTRIB_DIR)/drivers/video
 vpath %.c $(LX_CONTRIB_DIR)/drivers/video/fbdev/core
 vpath %.c $(LX_CONTRIB_DIR)/lib
+
+vpath %.c  $(SRC_DIR)
+vpath %.cc $(SRC_DIR)
+vpath %.cc $(REP_DIR)/src/lx_kit
 
---
+++ repos/dde_linux/src/lib/framebuffer/intel/main.cc
@@ -0,0 +1,121 @@
+/*
+ * \brief  Intel framebuffer driver
+ * \author Norman Feske
+ * \author Stefan Kalkowski
+ * \date   2015-08-19
+ */
+
+/*
+ * Copyright (C) 2015 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+/* Genode includes */
+#include <base/log.h>
+#include <base/component.h>
+#include <base/attached_rom_dataspace.h>
+#include <os/config.h>
+
+/* Server related local includes */
+#include <component.h>
+
+/* Linux emulation environment includes */
+#include <lx_emul.h>
+#include <lx_kit/scheduler.h>
+#include <lx_kit/timer.h>
+#include <lx_kit/irq.h>
+#include <lx_kit/pci_dev_registry.h>
+#include <lx_kit/backend_alloc.h>
+#include <lx_kit/work.h>
+
+/* Linux module functions */
+extern "C" int postcore_i2c_init(); /* i2c-core.c */
+extern "C" int module_i915_init();  /* i915_drv.c */
+
+static void run_linux(void * m);
+
+unsigned long jiffies;
+
+
+struct Main
+{
+	Genode::Env                   &env;
+	Genode::Entrypoint            &ep     { env.ep() };
+	Genode::Attached_rom_dataspace config { env, "config" };
+	Genode::Heap                   heap   { env.ram(), env.rm() };
+	Framebuffer::Root              root   { env, heap, config };
+
+	/* Linux task that handles the initialization */
+	Genode::Constructible<Lx::Task> linux;
+
+	Main(Genode::Env &env) : env(env)
+	{
+		Genode::log("--- intel framebuffer driver ---");
+
+		Lx_kit::construct_env(env);
+
+		/* init singleton Lx::Scheduler */
+		Lx::scheduler(&env);
+
+		Lx::pci_init(env, env.ram(), heap);
+		Lx::malloc_init(env, heap);
+
+		/* init singleton Lx::Timer */
+		Lx::timer(&env, &ep, &heap, &jiffies);
+
+		/* init singleton Lx::Irq */
+		Lx::Irq::irq(&ep, &heap);
+
+		/* init singleton Lx::Work */
+		Lx::Work::work_queue(&heap);
+
+		linux.construct(run_linux, reinterpret_cast<void*>(this),
+		                "linux", Lx::Task::PRIORITY_0, Lx::scheduler());
+
+		/* give all task a first kick before returning */
+		Lx::scheduler().schedule();
+	}
+
+	void announce() { env.parent().announce(ep.manage(root)); }
+};
+
+
+struct Policy_agent
+{
+	Main &main;
+	Genode::Signal_rpc_member<Policy_agent> sd;
+
+	void handle(unsigned)
+	{
+		main.linux.unblock();
+		Lx::scheduler().schedule();
+	}
+
+	Policy_agent(Main &m)
+	: main(m), sd(main.ep, *this, &Policy_agent::handle) {}
+};
+
+
+static void run_linux(void * m)
+{
+	Main * main = reinterpret_cast<Main*>(m);
+
+	postcore_i2c_init();
+	module_i915_init();
+	main->root.session.driver().finish_initialization();
+	main->announce();
+
+	static Policy_agent pa(*main);
+	main->config.sigh(pa.sd);
+
+	while (1) {
+		Lx::scheduler().current()->block_and_schedule();
+		main->root.session.config_changed();
+	}
+}
+
+
+void start_framebuffer_driver(Genode::Env &env) {
+	static Main main (env); }
