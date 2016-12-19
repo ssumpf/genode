853671f086d2d70dc91edc25bc7dab327bac547c
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2016-12-19 15:18:49 +0100

    libports: hello gl

---
+++ repos/libports/src/test/hello-gl/target.mk
@@ -0,0 +1,19 @@
+TARGET = hello-gl
+LIBS   = libm libc egl mesa-11
+
+SRC_C  = hello-gl.c eglut.c util.c
+SRC_CC = eglut_genode.cc
+CC_OLEVEL = -O0
+EGLUT_DIR = $(REP_DIR)/src/lib/mesa/eglut
+
+INC_DIR += $(EGLUT_DIR) $(REP_DIR)/src/lib/mesa/include
+
+# XXX: Remove
+INC_DIR += $(REP_DIR)/../dde_linux/src/lib/framebuffer/intel/include
+INC_DIR += $(REP_DIR)/../dde_linux/src/include
+INC_DIR += $(REP_DIR)/../dde_linux/src/include/spec/x86_64
+LIBS += intel_fb
+
+vpath %.c  $(EGLUT_DIR)
+vpath %.cc $(EGLUT_DIR)
+
