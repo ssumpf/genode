dcb2bb8a558cf556e8d6776684078200c8ef1b74
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-02-07 18:27:33 +0100

    fixup! hw: bootstrap into kernel

--- repos/base-hw/lib/mk/spec/x86_64/bootstrap-hw-muen_on.mk
+++ repos/base-hw/lib/mk/spec/x86_64/bootstrap-hw-muen_on.mk
@@ -2,7 +2,7 @@ REQUIRES = muen
 
 INC_DIR += $(REP_DIR)/src/core/include/spec/x86_64/muen
 
-SRC_CC  += base/muen/sinfo.cc
+SRC_CC  += lib/muen/sinfo.cc
 SRC_CC  += bootstrap/spec/x86_64/platform_muen.cc
 SRC_S   += bootstrap/spec/x86_64/crt0_translation_table_muen.s
 
