199a19c033f7b2aaa31d149be3c1324c184d98cc
Martin Stein <martin.stein@genode-labs.com>
2017-02-15 14:08:10 +0100

    fixup "lxip: VFS plugin"
    vfs lxip: fix reading in LXIP local file
    
    The read on a LXIP local file formerly returned bogus because the VFS
    implementation wrote only to the local content buffer and not to the
    destination buffer.

--- repos/dde_linux/src/lib/vfs/lxip/vfs.cc
+++ repos/dde_linux/src/lib/vfs/lxip/vfs.cc
@@ -628,7 +628,7 @@ class Vfs::Lxip_local_file : public Vfs::Lxip_file
 			in_addr const i_addr   = addr->sin_addr;
 			unsigned char const *a = (unsigned char *)&i_addr.s_addr;
 			unsigned char const *p = (unsigned char *)&addr->sin_port;
-			return Genode::snprintf(_content_buffer, len,
+			return Genode::snprintf(dst, len,
 			                        "%d.%d.%d.%d:%u\n",
 			                        a[0], a[1], a[2], a[3], (p[0]<<8)|(p[1]<<0));
 		}
