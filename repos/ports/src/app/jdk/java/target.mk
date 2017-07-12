e5c3cb96bd3e8cdf6ab84f6f9f96d90da7de22df
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-07-12 14:16:55 +0200

    jdk: launcher + java.lib.so

--- repos/ports/src/app/jdk/java/target.mk
+++ repos/ports/src/app/jdk/java/target.mk
@@ -1,8 +1,8 @@
 TARGET   = java
 SRC_C    = main.c
-LIBS     = jli java jvm libc posix libc_pipe
+LIBS     = jli posix
 
-CC_C_OPT = -DVERSION_STRING='"9-genode.openjdk"' -D__GENODE__
+CC_C_OPT = -DVERSION_STRING='"9-genode.openjdk"'
 
 JDK_PATH = $(call select_from_ports,jdk)/src/app/jdk
 vpath main.c $(JDK_PATH)/jdk/src/java.base/share/native/launcher
