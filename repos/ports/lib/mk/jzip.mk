a1b65b3c1a164c856a41e90721e4ed14f5a7067e
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-09-07 11:05:54 +0200

    jdk: jimage and zip libraries

---
+++ repos/ports/lib/mk/jzip.mk
@@ -0,0 +1,15 @@
+LIBS       = libc zlib
+SHARED_LIB = yes
+JDK_BASE   = $(call select_from_ports,jdk)/src/app/jdk/jdk/src/java.base
+
+SRC_C = Adler32.c CRC32.c Deflater.c Inflater.c zip_util.c
+
+INC_DIR += $(JDK_BASE)/share/native/include \
+           $(JDK_BASE)/share/native/libjava \
+           $(JDK_BASE)/unix/native/include \
+           $(JDK_BASE)/unix/native/libjava \
+           $(REP_DIR)/src/app/jdk/lib/include/java.base
+
+CC_C_OPT = -D_ALLBSD_SOURCE
+
+vpath %.c $(JDK_BASE)/share/native/libzip
