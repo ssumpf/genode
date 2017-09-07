cc3c31c2f3d15795851a846944186933a0aafee0
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-09-07 11:05:54 +0200

    jdk: jimage and zip libraries

---
+++ repos/ports/lib/mk/jimage.mk
@@ -0,0 +1,20 @@
+LIBS       = libc
+SHARED_LIB = yes
+JDK_BASE   = $(call select_from_ports,jdk)/src/app/jdk/jdk/src/java.base
+
+SRC_CC = endian.cpp \
+         imageDecompressor.cpp \
+         imageFile.cpp \
+         jimage.cpp \
+         NativeImageBuffer.cpp \
+         osSupport_unix.cpp
+
+INC_DIR += $(JDK_BASE)/share/native/include \
+           $(JDK_BASE)/share/native/libjava \
+           $(JDK_BASE)/share/native/libjimage \
+           $(JDK_BASE)/unix/native/include \
+           $(JDK_BASE)/unix/native/libjava \
+           $(REP_DIR)/src/app/jdk/lib/include/java.base
+
+vpath %.cpp $(JDK_BASE)/share/native/libjimage
+vpath %.cpp $(JDK_BASE)/unix/native/libjimage
