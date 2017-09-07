d44536f1c27eff096c1101d23f8ffe848e4e6703
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-09-07 11:06:45 +0200

    jdk: make java a Libc::component
    + basic memory management

--- repos/ports/src/app/jdk/lib/jvm/dummies.cpp
+++ repos/ports/src/app/jdk/lib/jvm/dummies.cpp
@@ -1,5 +1,9 @@
 #include <base/log.h>
+#include <os/backtrace.h>
 
+extern "C" {
+#include <pwd.h>
+}
 #define WARN_NOT_IMPL Genode::warning(__func__, " not implemented");
 
 extern "C" void collector_func_load(char* name,
@@ -11,4 +15,17 @@ extern "C" void collector_func_load(char* name,
                                    void* null_argument_3)
 {
 	WARN_NOT_IMPL;
+}
+
+
+int getpwuid_r(uid_t uid, struct passwd *pwd,
+                      char *buf, size_t buflen, struct passwd **result)
+{
+	*result = nullptr;
+	return -1;
+}
+
+extern "C" void backtrace()
+{
+	Genode::backtrace();
 }
