6da7aea69a64c6cc459fae532d5fc32d8a46b9e0
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-09-07 11:05:54 +0200

    jdk: jimage and zip libraries

--- repos/ports/lib/mk/jimage.mk
+++ repos/ports/lib/mk/jimage.mk
@@ -1,9 +1,6 @@
-LIBS          = libc
-SHARED_LIB    = yes
-JDK_BASE      = $(call select_from_ports,jdk)/src/app/jdk/jdk/src/java.base
-JDK_GENERATED = $(call select_from_ports,jdk_generated)/src/app/jdk
-
-CC_CXX_WARN_STRICT =
+LIBS       = libc
+SHARED_LIB = yes
+JDK_BASE   = $(call select_from_ports,jdk)/src/app/jdk/jdk/src/java.base
 
 SRC_CC = endian.cpp \
          imageDecompressor.cpp \
@@ -17,7 +14,7 @@ INC_DIR += $(JDK_BASE)/share/native/include \
            $(JDK_BASE)/share/native/libjimage \
            $(JDK_BASE)/unix/native/include \
            $(JDK_BASE)/unix/native/libjava \
-           $(JDK_GENERATED)/include/java.base
+           $(REP_DIR)/src/app/jdk/lib/include/java.base
 
 vpath %.cpp $(JDK_BASE)/share/native/libjimage
 vpath %.cpp $(JDK_BASE)/unix/native/libjimage
--- repos/ports/lib/mk/jzip.mk
+++ repos/ports/lib/mk/jzip.mk
@@ -1,7 +1,6 @@
-LIBS          = libc zlib
-SHARED_LIB    = yes
-JDK_BASE      = $(call select_from_ports,jdk)/src/app/jdk/jdk/src/java.base
-JDK_GENERATED = $(call select_from_ports,jdk_generated)/src/app/jdk
+LIBS       = libc zlib
+SHARED_LIB = yes
+JDK_BASE   = $(call select_from_ports,jdk)/src/app/jdk/jdk/src/java.base
 
 SRC_C = Adler32.c CRC32.c Deflater.c Inflater.c zip_util.c
 
@@ -9,7 +8,7 @@ INC_DIR += $(JDK_BASE)/share/native/include \
            $(JDK_BASE)/share/native/libjava \
            $(JDK_BASE)/unix/native/include \
            $(JDK_BASE)/unix/native/libjava \
-           $(JDK_GENERATED)/include/java.base
+           $(REP_DIR)/src/app/jdk/lib/include/java.base
 
 CC_C_OPT = -D_ALLBSD_SOURCE
 
