45121ae3472533378b8872e8c1b391a7d8697242
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-10-24 11:16:47 +0200

    jdk: fix jtool.mk

--- repos/ports/lib/mk/jtool.mk
+++ repos/ports/lib/mk/jtool.mk
@@ -1,11 +1,20 @@
+ifeq ($(called_from_lib_mk),yes)
+
 JDK_TOOL_BASE   = $(BUILD_BASE_DIR)/tool/jdk
 JDK_CONTRIB_DIR = $(call select_from_ports,jdk)/src/app/jdk
+
+MAKEOVERRIDES := $(filter-out SPEC=%,$(filter-out VERBOSE=%,$(MAKEOVERRIDES)))
+unexport VERBOSE
+unexport SPEC
+
 
 #XXX: add different configure options for ARM
 $(JDK_TOOL_BASE)/.prepared:
 	@echo "Building jdk ..."
 	@mkdir -p $(JDK_TOOL_BASE)
-	unset VERBOSE && cd $(JDK_TOOL_BASE) && /bin/bash $(JDK_CONTRIB_DIR)/configure && make
+	cd $(JDK_TOOL_BASE) && /bin/bash $(JDK_CONTRIB_DIR)/configure && make
 	@touch $(JDK_TOOL_BASE)/.prepared
 
 all: $(JDK_TOOL_BASE)/.prepared
+
+endif
