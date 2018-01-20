9d040506e80ea56b9a517bd7cdaeccf58cf57bba
Pirmin Duss <pirmin@trimpim.ch>
2018-01-20 11:05:05 +0100

    stdcxx: fix src recipe
    fixes #2649

--- repos/libports/recipes/src/stdcxx/content.mk
+++ repos/libports/recipes/src/stdcxx/content.mk
@@ -2,7 +2,9 @@ MIRROR_FROM_REP_DIR := lib/mk/stdcxx-c++98.mk \
                        lib/mk/stdcxx.mk \
                        lib/import/import-stdcxx.mk
 
-content: $(MIRROR_FROM_REP_DIR) src/lib/stdcxx/target.mk
+MIRROR_FROM_PORT_DIR := src/lib/stdcxx
+
+content: $(MIRROR_FROM_REP_DIR) $(MIRROR_FROM_PORT_DIR) src/lib/stdcxx/target.mk
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
@@ -12,8 +14,6 @@ src/lib/stdcxx/target.mk:
 	echo "LIBS = stdcxx" > $@
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/stdcxx)
-
-MIRROR_FROM_PORT_DIR := src/lib/stdcxx
 
 content: $(MIRROR_FROM_PORT_DIR)
 
