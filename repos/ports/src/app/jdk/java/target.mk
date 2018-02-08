642592db137808b00fc477f9c6262cf88fa487bb
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2018-02-08 13:04:12 +0100

    Revert "jdk: remove gnerated files"
    This reverts commit 7ce01bb5dd9f8f5bbf95db9820f8c3dbdfd1968d.

--- repos/ports/src/app/jdk/java/target.mk
+++ repos/ports/src/app/jdk/java/target.mk
@@ -1,8 +1,8 @@
 TARGET   = java
 SRC_C    = main.c
-LIBS     = jtool jli java jvm libc
+LIBS     = jli java jvm libc
 
-CC_C_OPT = -DVERSION_STRING='"9-genode.openjdk"' -D__GENODE__
+CC_C_OPT = -DVERSION_STRING='"9-genode.openjdk"'
 
 JDK_PATH = $(call select_from_ports,jdk)/src/app/jdk
 vpath main.c $(JDK_PATH)/jdk/src/java.base/share/native/launcher
