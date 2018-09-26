a88873e585dafe07955f78d979ca134d55743967
Christian Helmuth <christian.helmuth@genode-labs.com>
2018-09-26 16:44:46 +0200

    Window manager test for tiled-console scenario

---
+++ repos/gems/src/test/tiled_wm/app/app.pro
@@ -0,0 +1,6 @@
+TEMPLATE   = app
+TARGET     = test-tiled_wm-app
+QT         = core gui widgets
+SOURCES   += main.cpp app.cpp
+HEADERS   += app.h util.h icon.h
+RESOURCES  = app.qrc
---
+++ repos/gems/src/test/tiled_wm/overlay/target.mk
@@ -0,0 +1,9 @@
+include $(call select_from_repositories,src/app/qt5/tmpl/target_defaults.inc)
+
+include $(call select_from_repositories,src/app/qt5/tmpl/target_final.inc)
+
+CC_CXX_WARN_STRICT =
+
+INC_DIR += $(PRG_DIR)/..
+
+#overlay.o: overlay.moc
