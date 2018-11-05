fbfafe519d05daa1fd4eb50400d47dae5f7e734d
Josef Söntgen <josef.soentgen@genode-labs.com>
2018-11-05 15:58:43 +0100

    vfs/terminal: use Device_file_system

--- repos/os/src/lib/vfs/terminal_file_system.h
+++ repos/os/src/lib/vfs/terminal_file_system.h
@@ -7,7 +7,7 @@
  */
 
 /*
- * Copyright (C) 2012-2017 Genode Labs GmbH
+ * Copyright (C) 2012-2018 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU Affero General Public License version 3.
@@ -16,29 +16,68 @@
 #ifndef _INCLUDE__VFS__TERMINAL_FILE_SYSTEM_H_
 #define _INCLUDE__VFS__TERMINAL_FILE_SYSTEM_H_
 
-#include <terminal_session/connection.h>
-#include <vfs/single_file_system.h>
-#include <base/signal.h>
+/* Genode includes */
 #include <base/registry.h>
+#include <base/signal.h>
+#include <terminal_session/connection.h>
+#include <util/xml_generator.h>
+#include <vfs/device_file_system.h>
+#include <vfs/readonly_value_file_system.h>
 
 
 namespace Vfs { class Terminal_file_system; }
 
 
-class Vfs::Terminal_file_system : public Single_file_system
+class Vfs::Terminal_file_system : public Device_file_system
 {
 	private:
 
+		Terminal_file_system(Terminal_file_system const &);
+		Terminal_file_system &operator = (Terminal_file_system const &);
+
 		typedef Genode::String<64> Label;
 		Label _label;
+		typedef Genode::String<64> Name;
+		Label _name;
 
-		Genode::Env         &_env;
-		Io_response_handler &_io_handler;
+		Vfs::Env               &_env;
+		Io_response_handler    &_io_handler;
+		Watch_response_handler &_watch_handler;
 
-		Terminal::Connection _terminal { _env, _label.string() };
+		Terminal::Connection _terminal { _env.env(), _label.string() };
 
-		typedef Genode::Registered<Vfs_handle>      Registered_handle;
-		typedef Genode::Registry<Registered_handle> Handle_registry;
+		struct Terminal_vfs_file_handle : public Device_vfs_handle
+		{
+			Terminal::Connection &_terminal;
+
+			Terminal_vfs_file_handle(Directory_service    &ds,
+			                    File_io_service      &fs,
+			                    Genode::Allocator    &alloc,
+			                    Terminal::Connection &terminal,
+			                    int flags)
+			: Device_vfs_handle(ds, fs, alloc, flags), _terminal(terminal) { }
+
+			Read_result read(char *dst, file_size count, file_size &out_count)
+			{
+				if (_terminal.avail()) {
+					out_count = _terminal.read(dst, count);
+					return READ_OK;
+				} else {
+					return READ_QUEUED;
+				}
+			}
+
+			Write_result write(char const *buf, file_size buf_size, file_size &out_count)
+			{
+				out_count = _terminal.write(buf, buf_size);
+				return WRITE_OK;
+			}
+
+			bool read_ready() { return _terminal.avail(); }
+		};
+
+		typedef Genode::Registered<Terminal_vfs_file_handle> Registered_handle;
+		typedef Genode::Registry<Registered_handle>          Handle_registry;
 
 		struct Post_signal_hook : Genode::Entrypoint::Post_signal_hook
 		{
@@ -78,12 +117,12 @@ class Vfs::Terminal_file_system : public Single_file_system
 				Post_signal_hook &operator = (Post_signal_hook const &);
 		};
 
-		Post_signal_hook _post_signal_hook { _env.ep(), _io_handler };
+		Post_signal_hook _post_signal_hook { _env.env().ep(), _io_handler };
 
 		Handle_registry _handle_registry { };
 
 		Genode::Io_signal_handler<Terminal_file_system> _read_avail_handler {
-			_env.ep(), *this, &Terminal_file_system::_handle_read_avail };
+			_env.env().ep(), *this, &Terminal_file_system::_handle_read_avail };
 
 		void _handle_read_avail()
 		{
@@ -92,82 +131,111 @@ class Vfs::Terminal_file_system : public Single_file_system
 			});
 		}
 
-		Read_result _read(Vfs_handle *, char *dst, file_size count,
-		                  file_size &out_count)
+		using Config = Genode::String<256>;
+		static Config _dir_config(Genode::Xml_node node)
 		{
-			if (_terminal.avail()) {
-				out_count = _terminal.read(dst, count);
-				return READ_OK;
-			} else {
-				return READ_QUEUED;
+			char buf[Config::capacity()] { };
+
+			Genode::Xml_generator xml(buf, sizeof(buf), "dir", [&] {
+
+				using Name = Genode::String<64>;
+				Name dir_name = node.attribute_value("name", Name(name()));
+
+				xml.attribute("name", Name(".", dir_name));
+
+				xml.node("readonly_value", [&] {
+					xml.attribute("name", "window_size");
+				});
+			});
+			return Config(Genode::Cstring(buf));
+		}
+
+		using Wsize = Terminal::Session::Size;
+		Readonly_value_file_system<Wsize> _window_size_fs {
+			_env, "window_size", Wsize(0, 0) };
+
+		Vfs::File_system *create(Vfs::Env &, Genode::Xml_node node) override
+		{
+			if (node.has_type(Readonly_value_file_system<Wsize>::type_name())) {
+				return _window_size_fs.matches(node) ? &_window_size_fs : nullptr;
 			}
+
+			return nullptr;
+		}
+
+		Genode::Io_signal_handler<Terminal_file_system> _size_changed_handler {
+			_env.env().ep(), *this, &Terminal_file_system::_handle_size_changed };
+
+		void _handle_size_changed()
+		{
+			_window_size_fs.value(_terminal.size());
 		}
 
 	public:
 
 		Terminal_file_system(Vfs::Env &env, Genode::Xml_node config)
 		:
-			Single_file_system(NODE_TYPE_CHAR_DEVICE, name(), config),
+			Device_file_system(NODE_TYPE_CHAR_DEVICE, name(), config),
 			_label(config.attribute_value("label", Label())),
-			_env(env.env()), _io_handler(env.io_handler())
+			_name(config.attribute_value("name", Name(name()))),
+			_env(env), _io_handler(env.io_handler()),
+			_watch_handler(env.watch_handler())
 		{
+			Device_file_system::construct(env, Genode::Xml_node(_dir_config(config).string()), *this);
+
 			/* register for read-avail notification */
 			_terminal.read_avail_sigh(_read_avail_handler);
+
+			/* register for size-change notification */
+			_terminal.size_changed_sigh(_size_changed_handler);
+			_handle_size_changed();
 		}
 
 		static const char *name()   { return "terminal"; }
 		char const *type() override { return "terminal"; }
 
-		Open_result open(char const  *path, unsigned,
+		/*********************************
+		 ** Directory service interface **
+		 *********************************/
+
+		Open_result open(char const  *path, unsigned flags,
 		                 Vfs_handle **out_handle,
 		                 Allocator   &alloc) override
 		{
-			if (!_single_file(path))
-				return OPEN_ERR_UNACCESSIBLE;
-
 			try {
-				*out_handle = new (alloc)
-					Registered_handle(_handle_registry, *this, *this, alloc, 0);
-				return OPEN_OK;
+				if (_device_file(path)) {
+					*out_handle = new (alloc)
+						Registered_handle(_handle_registry, *this, *this,
+						                  alloc, _terminal, flags);
+					return OPEN_OK;
+				} else
+
+				if (_device_dir_file(path)) {
+					return Device_file_system::open(path, 0, out_handle, alloc);
+				}
 			}
 			catch (Genode::Out_of_ram)  { return OPEN_ERR_OUT_OF_RAM; }
 			catch (Genode::Out_of_caps) { return OPEN_ERR_OUT_OF_CAPS; }
+
+			return OPEN_ERR_UNACCESSIBLE;
 		}
 
 		/********************************
 		 ** File I/O service interface **
 		 ********************************/
 
-		Write_result write(Vfs_handle *, char const *buf, file_size buf_size,
-		                   file_size &out_count) override
-		{
-			out_count = _terminal.write(buf, buf_size);
-			return WRITE_OK;
-		}
-
-		Read_result complete_read(Vfs_handle *vfs_handle, char *dst, file_size count,
-		                          file_size &out_count) override
-		{
-			return _read(vfs_handle, dst, count, out_count);
-		}
-
-		bool read_ready(Vfs_handle *) override
-		{
-			return _terminal.avail();
-		}
-
 		Ftruncate_result ftruncate(Vfs_handle *, file_size) override
 		{
 			return FTRUNCATE_OK;
 		}
 
-		bool check_unblock(Vfs_handle *, bool rd, bool wr, bool) override
+		bool check_unblock(Vfs_handle *vfs_handle, bool rd, bool wr, bool) override
 		{
-			if (rd && (_terminal.avail() > 0))
-				return true;
+			Device_vfs_handle *handle =
+				static_cast<Device_vfs_handle*>(vfs_handle);
 
-			if (wr)
-				return true;
+			if (rd && handle->read_ready()) { return true; }
+			if (wr)                         { return true; }
 
 			return false;
 		}
@@ -177,6 +245,7 @@ class Vfs::Terminal_file_system : public Single_file_system
 		{
 			_terminal.read_avail_sigh(sigh);
 		}
+
 };
 
 #endif /* _INCLUDE__VFS__TERMINAL_FILE_SYSTEM_H_ */
