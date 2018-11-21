df6c315dc51edee76b94365459a0b2e46707263e
Josef Söntgen <josef.soentgen@genode-labs.com>
2018-11-21 15:37:38 +0100

    XXX use speex resample copy from libopusenc -- REMOVE LATER

--- repos/gems/lib/mk/vfs_oss.mk
+++ repos/gems/lib/mk/vfs_oss.mk
@@ -2,4 +2,12 @@ SRC_CC := vfs_oss.cc
 
 vpath %.cc $(REP_DIR)/src/lib/vfs/oss
 
+
+SRC_C   := resample.c
+INC_DIR += $(REP_DIR)/src/lib/vfs/oss/contrib
+LIBS    += libc libm
+CC_OPT  += -DOUTSIDE_SPEEX -DRANDOM_PREFIX=genode
+vpath %.c  $(REP_DIR)/src/lib/vfs/oss/contrib
+
+
 SHARED_LIB := yes
