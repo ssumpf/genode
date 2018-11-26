0131c95e3d011d48b9168edb7edea940e887a9c3
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2018-11-26 11:18:57 +0100

    hw: improve cross-cpu synchronization
    This commit addresses several multiprocessing issues in base-hw:
    
    * it reworks cross-cpu maintainance work for TLB invalidation by
      introducing a generic Inter_processor_work and removes the so
      called Cpu_domain_update
    * thereby it solves the cross-cpu thread destruction, when the
      corresponding thread is active on another cpu (fix #3043)
    * it adds the missing TLB shootdown for x86 (fix #3042)
    * on ARM it removes the TLB shootdown via IPIs, because this
      is not needed on the multiprocessing ARM platforms we support
    * it enables the per-cpu initialization of the kernel's cpu
      objects, which means those object initialization is executed
      by the proper cpu
    * it rollbacks prior decision to make multiprocessing an aspect,
      but puts back certain 'smp' mechanisms (like cross-cpu lock)
      into the generic code base for simplicity reasons

--- repos/base-hw/src/lib/hw/spec/arm/cpu.h
+++ repos/base-hw/src/lib/hw/spec/arm/cpu.h
@@ -193,8 +193,14 @@ struct Hw::Arm_cpu
 	/* Invalidate entire unified TLB */
 	ARM_CP15_REGISTER_32BIT(Tlbiall, c8, c7, 0, 0);
 
+	/* Invalidate entire unified TLB (inner-shareable) */
+	ARM_CP15_REGISTER_32BIT(Tlbiallis, c8, c3, 0, 0);
+
 	/* Invalidate unified TLB by ASID */
 	ARM_CP15_REGISTER_32BIT(Tlbiasid, c8, c7, 0, 2);
+
+	/* Invalidate unified TLB by ASID (inner-shareable) */
+	ARM_CP15_REGISTER_32BIT(Tlbiasidis, c8, c3, 0, 2);
 
 	/* Memory Attribute Indirection Register 0 */
 	ARM_CP15_REGISTER_32BIT(Mair0, c10, c2, 0, 0,
