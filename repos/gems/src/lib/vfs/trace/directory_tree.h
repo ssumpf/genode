ac71b33326db2e9bd4fbfafc5ee85ce1aee4f328
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2019-04-15 15:49:24 +0200

    vfs_trace: directory tree working

--- repos/gems/src/lib/vfs/trace/directory_tree.h
+++ repos/gems/src/lib/vfs/trace/directory_tree.h
@@ -6,13 +6,9 @@
 #include "session_label.h"
 
 namespace Vfs {
-	class Directory_tree;
-	template <typename> class Object_tree;
-
-	class String_node;
-	class Trace_node;
-
-	typedef Genode::Avl_string<32> Label;
+	class  Directory_tree;
+	class  Trace_node;
+	struct Label;
 }
 
 namespace Genode {
@@ -25,7 +21,7 @@ class Genode::Avl_node_tree : public NT
 	protected:
 
 		using Tree = Avl_tree<NT>;
-		using Node = NT;
+		using Node = Avl_node<NT>;
 
 		Tree _tree;
 
@@ -36,68 +32,71 @@ class Genode::Avl_node_tree : public NT
 		void insert(Node *node) { _tree.insert(node); }
 
 		Tree &tree() { return _tree; }
-};
 
-class Vfs::Trace_node : public Vfs::Label
-{
-	private:
-
-		Trace::Subject_id const _id;
-
-	public:
-
-		Trace_node(Trace::Subject_id const id, char const *name)
-		: Avl_string(name),
-		  _id(id) { }
-};
-
-
-class Vfs::String_node : public Avl_node_tree<Vfs::Label>
-{
-	private:
-
-		Allocator &_alloc;
-
-		String_node *_find_by_name(char const *name)
+		Node *find_by_name(char const *name)
 		{
 			if (!_tree.first()) return nullptr;
 
-			Avl_string_base *node = _tree.first()->find_by_name(name);
-			if (!node) return nullptr;
+			Node *node = _tree.first()->find_by_name(name);
+			return node;
+		}
+};
 
-			return static_cast<String_node *>(node);
+struct Vfs::Label : Genode::String<32>
+{
+	using String::String;
+};
+
+class Vfs::Trace_node : public Vfs::Label,
+                         public Avl_node_tree<Genode::Avl_string_base>
+{
+	private:
+
+		Allocator              &_alloc;
+		Trace::Subject_id const _id;
+
+		Trace_node *_find_by_name(char const *name)
+		{
+			Node *node = find_by_name(name);
+			return node ? static_cast<Trace_node *>(node) : nullptr;
 		}
 
 	public:
 
-		String_node(Genode::Allocator &alloc, Session_label const &label)
-		: Avl_node_tree(label.string()),
-		  _alloc(alloc) { Genode::warning("NEW node: ", label); }
+		Trace_node(Genode::Allocator &alloc, Session_label const &label,
+		            Trace::Subject_id const id = 0)
+		: Vfs::Label(label), Avl_node_tree(string()),
+		  _alloc(alloc), _id(id)
+		{ }
 
-		String_node &insert(Session_label const &label)
+		Trace_node &insert(Session_label const &label)
 		{
-			if (!label.valid()) { log("NOT VALID"); return *this; }
+			if (!label.valid()) return *this;
 
-			String_node *node = _find_by_name(label.first_element().string());
-			Genode::warning("search: ", label.first_element(), " node: ", node, " node name: ", node ? node->name() : "<empty>");
+			Trace_node *node = _find_by_name(label.first_element().string());
 			if (!node) {
-				node = new(_alloc) String_node(_alloc, label.first_element());
+				node = new(_alloc) Trace_node(_alloc, label.first_element());
 				Avl_node_tree::insert(node);
-				Genode::log("insert: ", label.first_element());
 			}
 
-			Genode::error("suffix: ", label.suffix(), " node: ", node->name());
 			return node->insert(label.suffix());
+		}
+
+		void xml(unsigned level)
+		{
+			_tree.for_each([&] (Genode::Avl_string_base const &node) {
+				Trace_node &n = const_cast<Trace_node &>(static_cast<Trace_node const &>(node));
+				Genode::warning("(", level, ") xml: ", node.name(), " id: ", n._id.id);
+				n.xml(level + 1);});
 		}
 };
 
-
-class Vfs::Directory_tree
+class Vfs::Directory_tree : public Genode::Avl_tree<Trace_node>
 {
 	private:
 
 		Allocator     &_alloc;
-		String_node    _root { _alloc, Session_label() };
+		Trace_node    _root { _alloc, Session_label() };
 
 	public:
 
@@ -106,79 +105,15 @@ class Vfs::Directory_tree
 
 		void insert(Trace::Subject_info const &info, Trace::Subject_id const id)
 		{
-			String_node &leaf = _root.insert(info.session_label());
-
-			Trace_node *node = new (_alloc) Trace_node(id, info.thread_name().string());
-			Genode::warning("insert leaf tree: ", &leaf.tree(), " thread: ", info.thread_name());
-			leaf.insert(node);
-			Genode::warning("leaf done");
+			Trace_node &leaf = _root.insert(info.session_label());
+			Trace_node *node  = new (_alloc) Trace_node(_alloc, info.thread_name(), id);
+			leaf.Avl_node_tree::insert(node);
 		}
 
 		void xml()
 		{
-			_root.tree().for_each([&] (Genode::Avl_string_base const &node) {
-				Genode::warning("node ", &node);
-				Genode::warning("for: ", node.name());
-			});
+			_root.xml(0);
 		}
 };
 
-#if 0
-template <typename OBJECT>
-class Vfs::Object_tree : public Genode::Avl_tree<Genode::Avl_string_base>
-{
-
-};
-
-class Vfs::Directory_tree : public Genode::Avl_tree<Avl_string_base>,
-                            public Genode::Avl_string_base
-{
-	private:
-
-		using Node = Genode::Avl_string_base;
-		using Tree = Genode::Avl_tree<Avl_string_base>;
-
-		Allocator     &_alloc;
-
-		Directory_tree *_find_by_name(char const *name)
-		{
-			if (!first()) return nullptr;
-
-			Node *node = first()->find_by_name(name);
-			if (!node) return nullptr;
-
-			return static_cast<Directory_tree *>(node);
-		}
-
-		Directory_tree *_insert(Session_label const &name)
-		{
-			Directory_tree *next_level = _find_by_name(name.string());
-
-			if (next_level) return next_level;
-
-			next_level = new (_alloc) Directory_tree(_alloc, name);
-			Tree::insert(static_cast<Node *>(next_level));
-
-			return next_level;
-		}
-
-	public:
-
-		Directory_tree(Genode::Allocator &alloc, Session_label const &name)
-
-		: Avl_string_base(name.string()),
-		  _alloc(alloc) { }
-
-		void insert(Vfs::Session_label const &label)
-		{
-			Directory_tree *next_level    = this;
-			Session_label   current       = label;
-
-			do {
-				next_level = next_level->_insert(current.first_element());
-				current = current.suffix();
-			} while (current.valid());
-		}
-};
-#endif
 #endif /* _DIRECTORY_TREE_H_ */
