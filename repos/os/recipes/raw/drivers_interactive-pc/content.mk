6b0a500432a7775ef20d7b4f82e94dc90fb25592
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2021-05-07 16:30:27 +0200

    mesa*.run: make runnable on hardware
    Issue #4148

--- repos/os/recipes/raw/drivers_interactive-pc/content.mk
+++ repos/os/recipes/raw/drivers_interactive-pc/content.mk
@@ -1,6 +1,7 @@
-content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen
+content: drivers.config fb_drv.config event_filter.config en_us.chargen \
+         special.chargen drivers_platform.config
 
-drivers.config fb_drv.config event_filter.config:
+drivers.config fb_drv.config event_filter.config drivers_platform.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-pc/$@ $@
 
 en_us.chargen special.chargen:
