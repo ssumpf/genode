c68290eaaef5630ab5da3bf31a7582eb625e0b45
Josef Söntgen <josef.soentgen@genode-labs.com>
2021-06-25 13:44:32 +0200

    imx8q_gpu: add generated Linux header files
    This header files should probably either moved to a external repository
    and be pulled in by the port mechanism or they should be generated by
    employing the Linux tools.
    
    Either way, for now store them in this repository itself.

---
+++ repos/imx8q/src/drivers/gpu/etnaviv/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 17 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#17 SMP PREEMPT Tue Apr 6 14:06:13 CEST 2021"
+#define LINUX_COMPILE_BY "jws"
+#define LINUX_COMPILE_HOST "develop"
+#define LINUX_COMPILER "gcc version 9.3.0 (Buildroot 2020.08-14-ge5a2a90)"
---
+++ repos/imx8q/src/drivers/gpu/etnaviv/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "5.4.104-gd45e41461c0a-dirty"
