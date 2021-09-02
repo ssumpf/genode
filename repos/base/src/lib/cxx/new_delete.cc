f3ba0df101340b615e0ba15dc83a68cda8c1fc60
Christian Helmuth <christian.helmuth@genode-labs.com>
2021-09-02 15:04:04 +0200

    fixup "cxx: define delete operator with alignm. arg" (fix coding style en passant)

--- repos/base/src/lib/cxx/new_delete.cc
+++ repos/base/src/lib/cxx/new_delete.cc
@@ -73,11 +73,13 @@ __attribute__((weak)) void operator delete (void *) noexcept
 	              "A working implementation is available in the 'stdcxx' library.");
 }
 
+
 __attribute__((weak)) void operator delete (void *, unsigned long)
 {
 	Genode::error("cxx: operator delete (void *, unsigned long) called - not implemented. "
 	              "A working implementation is available in the 'stdcxx' library.");
 }
+
 
 __attribute__((weak)) void operator delete (void *, unsigned long, std::align_val_t)
 {
