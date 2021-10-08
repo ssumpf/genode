395ae7560011719940509334fdca224ed78bb16f
Norman Feske <norman.feske@genode-labs.com>
2021-10-08 10:57:22 +0200

    drivers_interactive-pc: remove Gpu service
    This patch reverts the commit "driver_interactive-pc: start GPU
    multiplexer". The addition of the GPU service introduces inconsistencies
    with boards other than PC, and increases the quota demand of the drivers
    subsystem, which breaks run scripts like demo.run on OKL4 or seL4.
    
    The generic drivers_interactive subsystems should remain deliberately
    limited to plain input and framebuffer devices. To cater additional
    peripherals like networking, audio, or GPU, we should use Sculpt as
    testbed or create board-specific run scripts.
    
    Issue #4260

--- repos/os/recipes/raw/drivers_interactive-pc/content.mk
+++ repos/os/recipes/raw/drivers_interactive-pc/content.mk
@@ -1,10 +1,7 @@
-content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen gpu_drv.config
+content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen
 
 drivers.config fb_drv.config event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-pc/$@ $@
-
-gpu_drv.config:
-	cp $(REP_DIR)/src/drivers/gpu/intel/$@ $@
 
 en_us.chargen special.chargen:
 	cp $(REP_DIR)/src/server/event_filter/$@ $@
