378c5e4f6fc3401dc0a03461527f33972a9c5dee
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2021-10-13 13:44:15 +0200

    Revert "drivers_interactive-pc: remove Gpu service"
    This reverts commit 395ae7560011719940509334fdca224ed78bb16f.

--- repos/os/recipes/raw/drivers_interactive-pc/content.mk
+++ repos/os/recipes/raw/drivers_interactive-pc/content.mk
@@ -1,7 +1,10 @@
-content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen
+content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen gpu_drv.config
 
 drivers.config fb_drv.config event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-pc/$@ $@
+
+gpu_drv.config:
+	cp $(REP_DIR)/src/drivers/gpu/intel/$@ $@
 
 en_us.chargen special.chargen:
 	cp $(REP_DIR)/src/server/event_filter/$@ $@
