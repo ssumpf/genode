cc309880bb101928af142eacade458283aa5ef3e
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-04-01 14:24:51 +0200

    dde_bsd: open playback device only in error case
    * For some audio devices the OpenBSD kernel does not have working capture
      device drivers, like the DMICs found in Intel HD Tigerlake. In that case
      we shall fallback to playback only.
    
    Fix genodelabs/genode#4469

--- repos/dde_bsd/src/lib/audio/driver.cc
+++ repos/dde_bsd/src/lib/audio/driver.cc
@@ -327,6 +327,11 @@ static bool open_audio_device(dev_t dev)
 		return false;
 
 	int err = audioopen(dev, FWRITE|FREAD, 0 /* ifmt */, 0 /* proc */);
+
+	/* try to open playback only, if capturing potentially failed */
+	if (err == ENODEV)
+		err = audioopen(dev, FWRITE, 0 /* ifmt */, 0 /* proc */);
+
 	if (err)
 		return false;
 
