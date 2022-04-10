7652a13d3a3dec9ee0d649ab1fa30168f1cdb8b8
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-04-10 02:13:58 +0200

    dde_rump: move periodic sync into rump kernel thread
    Issue #4459

---
+++ repos/dde_rump/src/lib/rump/sync_thread.c
@@ -0,0 +1,32 @@
+/*
+ * \brief  Rump kernel thread which syncs the file system every 10s
+ * \author Christian Prochaska
+ * \date   2022-04-08
+ */
+
+/*
+ * Copyright (C) 2022 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#include <sys/param.h>
+#include <rump/rumpuser.h>
+#include <sys/vfs_syscalls.h>
+
+extern void rump_io_backend_sync();
+
+void genode_sync_thread(void *arg)
+{
+	for (;;) {
+		/* sleep for 10 seconds */
+		rumpuser_clock_sleep(RUMPUSER_CLOCK_RELWALL, 10, 0);
+
+		/* sync through front-end */
+		do_sys_sync(curlwp);
+
+		/* sync Genode back-end */
+		rump_io_backend_sync();
+	}
+}
