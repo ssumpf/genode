81b581b58f5c7d8c79ea91422d5f34933864dda5
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-04-27 15:55:24 +0200

    platform_drv: separate devices info from config
    Fix genodelabs/genode#4491

--- repos/os/recipes/raw/drivers_interactive-pbxa9/content.mk
+++ repos/os/recipes/raw/drivers_interactive-pbxa9/content.mk
@@ -1,4 +1,7 @@
-content: drivers.config event_filter.config en_us.chargen special.chargen
+content: drivers.config event_filter.config en_us.chargen special.chargen devices
+
+devices:
+	cp $(BASE_DIR)/board/pbxa9/$@ $@
 
 drivers.config event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-pbxa9/$@ $@
--- repos/os/recipes/raw/drivers_nic-pbxa9/content.mk
+++ repos/os/recipes/raw/drivers_nic-pbxa9/content.mk
@@ -1,4 +1,7 @@
-content: drivers.config
+content: drivers.config devices
+
+devices:
+	cp $(BASE_DIR)/board/pbxa9/$@ $@
 
 drivers.config:
 	cp $(REP_DIR)/recipes/raw/drivers_nic-pbxa9/$@ $@
