90d08caf3df077998decfe7dd7cfe2057b5dfd7a
Norman Feske <norman.feske@genode-labs.com>
2022-05-20 16:08:33 +0200

    menu_view: menu_view_styles.tar is build artifact
    By declaring the generated menu_view_styles.tar archive as build
    artifact, it is automatically picked up whenever menu_view appears as
    build ingredient of sculpt.
    
      build: app/menu_view

--- repos/gems/src/app/menu_view/target.mk
+++ repos/gems/src/app/menu_view/target.mk
@@ -4,6 +4,7 @@ LIBS     = base libc libm vfs libpng zlib blit file
 INC_DIR += $(PRG_DIR)
 
 CUSTOM_TARGET_DEPS += menu_view_styles.tar
+BUILD_ARTIFACTS    += menu_view_styles.tar
 
 menu_view_styles.tar:
 	$(VERBOSE)cd $(PRG_DIR); tar cf $(PWD)/bin/$@ styles
