c4a818493e308c2a3d2db7bd3d0f70b5704f250f
Christian Helmuth <christian.helmuth@genode-labs.com>
2022-07-11 15:18:35 +0200

    lx_emul: cleanup naming of lx_emul_random_gen*
    Issue #4544

--- repos/dde_linux/src/app/wireguard/lx_emul.c
+++ repos/dde_linux/src/app/wireguard/lx_emul.c
@@ -20,7 +20,7 @@
 
 void get_random_bytes(void * buf,int nbytes)
 {
-	lx_emul_gen_random_bytes(buf, nbytes);
+	lx_emul_random_gen_bytes(buf, nbytes);
 }
 
 
@@ -37,7 +37,7 @@ int wait_for_random_bytes(void)
 
 u32 get_random_u32(void)
 {
-	return lx_emul_gen_random_u32();
+	return lx_emul_random_gen_u32();
 }
 
 
@@ -45,7 +45,7 @@ u32 get_random_u32(void)
 
 u32 prandom_u32(void)
 {
-	return lx_emul_gen_random_u32();
+	return lx_emul_random_gen_u32();
 }
 
 
@@ -53,7 +53,7 @@ u32 prandom_u32(void)
 
 int __must_check get_random_bytes_arch(void * buf, int nbytes)
 {
-	lx_emul_gen_random_bytes(buf, nbytes);
+	lx_emul_random_gen_bytes(buf, nbytes);
 	return nbytes;
 }
 
