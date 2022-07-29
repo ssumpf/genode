091ec508533b8cbc86a5b0459313365e5f4623f6
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-07-29 17:31:20 +0200

    lx_kit & lx_emul: use generic platform API
    * Remove wrapper for legacy x86 platform API
    * Move PCI configuration space quirks to corresponding driver
      (pc_usb_host_drv, pc_wifi_drv, pc_intel_fb_drv)
    * Adapt driver test run-scripts to changed configuration
    
    Ref genodelabs/genode#4578

--- repos/pc/src/drivers/usb_host/pc/lx_emul.c
+++ repos/pc/src/drivers/usb_host/pc/lx_emul.c
@@ -12,3 +12,59 @@
  */
 
 #include <lx_emul.h>
+#include <linux/pci.h>
+
+enum {
+	UHCI_USBLEGSUP          = 0xc0,
+	UHCI_USBRES_INTEL       = 0xc4,
+	EHCI_SERIAL_BUS_RELEASE = 0x60,
+	EHCI_PORT_WAKE          = 0x62,
+};
+
+
+int pci_read_config_byte(const struct pci_dev * dev,int where,u8 * val)
+{
+	switch (where) {
+	case EHCI_SERIAL_BUS_RELEASE:
+		*val = 0;
+		return 0;
+	};
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+int pci_read_config_word(const struct pci_dev * dev,int where,u16 * val)
+{
+	switch (where) {
+	case EHCI_PORT_WAKE:
+		*val = 0;
+		return 0;
+	case UHCI_USBLEGSUP:
+		/* force the driver to do a full_reset */
+		*val = 0xffff;
+		return 0;
+	};
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+int pci_write_config_byte(const struct pci_dev * dev,int where,u8 val)
+{
+	switch (where) {
+	case UHCI_USBRES_INTEL:
+		/* do nothing */
+		return 0;
+	}
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+int pci_write_config_word(const struct pci_dev * dev,int where,u16 val)
+{
+	switch (where) {
+	case UHCI_USBLEGSUP:
+		/* do nothing */
+		return 0;
+	}
+	lx_emul_trace_and_stop(__func__);
+}
