cb4017d11e93dca2c831664ad3656aabb4d0ad3b
Martin Stein <martin.stein@genode-labs.com>
2022-09-16 11:26:52 +0200

    nic_router: use the dictionary data structure
    This commit gets rid of the router-local wrapper of Genode's AVL string tree
    and replaces it with Genode's new Dictionary structure. The Dictionary is now
    used for managing domains and NIC clients. Due to this change, the formerly
    necessary helper classes Domain_base and Nic_client_base could be removed as
    well.
    
    Ref #4610

---
+++ repos/os/src/server/nic_router/dictionary.h
@@ -0,0 +1,58 @@
+/*
+ * \brief  Local convenience wrapper for the Genode dictionary
+ * \author Martin Stein
+ * \date   2022-09-16
+ */
+
+/*
+ * Copyright (C) 2022 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _DICTIONARY_H_
+#define _DICTIONARY_H_
+
+/* Genode includes */
+#include <base/allocator.h>
+#include <util/dictionary.h>
+
+namespace Net { template <typename, typename> class Dictionary; }
+
+
+template <typename OBJECT_T,
+          typename NAME_T>
+
+class Net::Dictionary : public Genode::Dictionary<OBJECT_T, NAME_T>
+{
+	private:
+
+		using Dict = Genode::Dictionary<OBJECT_T, NAME_T>;
+
+	public:
+
+		template <typename FUNCTION_T>
+		void for_each(FUNCTION_T const &function) const
+		{
+			Dict::for_each(
+				[&] (OBJECT_T const &obj)
+				{
+					function(*const_cast<OBJECT_T *>(&obj));
+				}
+			);
+		}
+
+		void destroy_each(Genode::Deallocator &dealloc)
+		{
+			while (
+				this->apply_any(
+					[&] (OBJECT_T &obj)
+					{
+						destroy(dealloc, &obj);
+					}))
+			{ }
+		}
+};
+
+#endif /* _DICTIONARY_H_ */
