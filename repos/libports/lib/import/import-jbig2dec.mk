ec3f2ddea49188bb86f73603c32bea9af34611bc
Christian Helmuth <christian.helmuth@genode-labs.com>
2022-10-14 13:57:42 +0200

    fixup "libs: use `select_from_ports` in eager assignments"
    libuvc:   aliasing of LIBUVC_DIR with different value
    jbig2dec: remove redundant $
    
    Issue genodelabs/genode#4618

--- repos/libports/lib/import/import-jbig2dec.mk
+++ repos/libports/lib/import/import-jbig2dec.mk
@@ -1,2 +1,2 @@
 JBIG2DEC_DIR := $(call select_from_ports,jbig2dec)
-INC_DIR += $($JBIG2DEC_DIR)/include/jbig2dec
+INC_DIR += $(JBIG2DEC_DIR)/include/jbig2dec
--- repos/libports/lib/import/import-libuvc.mk
+++ repos/libports/lib/import/import-libuvc.mk
@@ -1,2 +1,2 @@
-LIBUVC_DIR := $(call select_from_ports,libuvc)
-INC_DIR += $(LIBUVC_DIR)/include
+LIBUVC_PORT_DIR := $(call select_from_ports,libuvc)
+INC_DIR += $(LIBUVC_PORT_DIR)/include
