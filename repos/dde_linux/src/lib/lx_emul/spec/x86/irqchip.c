accdf6e7dd78c38afb10f469f5377d470c4d4571
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-10-26 17:28:30 +0200

    lx_emul: allow different irqchip declarations
    To enable other irqchip declarations beyond the lx_emul generic code base,
    make the ARM specific initialization routine public.
    
    Ref genodelabs/genode#4651

--- repos/dde_linux/src/lib/lx_emul/spec/x86/irqchip.c
+++ repos/dde_linux/src/lib/lx_emul/spec/x86/irqchip.c
@@ -31,6 +31,9 @@ static void dde_irq_mask(struct irq_data *d)
 }
 
 
+int lx_emul_irq_init(struct device_node *node, struct device_node *parent) {}
+
+
 struct irq_chip dde_irqchip_data_chip = {
 	.name           = "dde-irqs",
 	.irq_mask       = dde_irq_mask,
