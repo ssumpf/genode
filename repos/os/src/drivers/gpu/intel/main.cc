bfcea12524a13882646bedbca6d506c659937762
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2022-11-07 15:37:18 +0100

    gpu/intel: update caps + ram required for buffer allocations
    'manage' may conumes 4+ caps and 16+KB in 'Rpc_entrypoint::_alloc_rpc_cap'

--- repos/os/src/drivers/gpu/intel/main.cc
+++ repos/os/src/drivers/gpu/intel/main.cc
@@ -1593,14 +1593,15 @@ class Gpu::Session_component : public Genode::Session_object<Gpu::Session>
 			{ }
 
 			/* worst case */
-			bool avail_caps() { return _cap_quota_guard.have_avail(Cap_quota { 10 }); }
+			bool avail_caps() { return _cap_quota_guard.have_avail(Cap_quota { 15 }); }
 
 			/* size + possible heap allocations  + possible page table allocation +
-			 * unkown overhead */
+			 * + 16KB for ep.manage unkown overhead */
 			bool avail_ram(size_t size = 0) {
-				return _ram_quota_guard.have_avail(Ram_quota { size + 2*1024*1024+4096 + 1024*1024 + 1024*1024}); }
+				return _ram_quota_guard.have_avail(Ram_quota { size + 2*1024*1024+4096 +
+				                                               1024*1024 + 16*1024 + 1024*1024}); }
 			size_t test_ram(size_t size) {
-				return size + 2*1024*1024+4096 + 1024*1024 + 1024*1024;
+				return size + 2*1024*1024+4096 + 1024*1024 + 16*1024 * 1024*1024;
 			}
 			void withdraw(size_t caps_old, size_t caps_new,
 			              size_t ram_old, size_t ram_new)
