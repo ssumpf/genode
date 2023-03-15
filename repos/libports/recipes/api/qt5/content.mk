ad9d9b74ad079d2c9d601d039e5c8337e6ec002e
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-03-15 09:12:19 +0100

    qt5: split qt5 api
    Split api into 'qt5', 'qt5_qml', and 'qt5_webkit'. This makes it
    possible to link Qt5 apps more fine grained.

--- repos/libports/recipes/api/qt5/content.mk
+++ repos/libports/recipes/api/qt5/content.mk
@@ -8,12 +8,33 @@ $(MIRROR_FROM_REP_DIR):
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/qt5)
 
-MIRROR_FROM_PORT_DIR := src/lib/qt5/genode/api/*
+MIRROR_FROM_PORT_DIR := src/lib/qt5/genode/api/include \
+                        src/lib/qt5/genode/api/mkspecs \
+                        src/lib/qt5/genode/api/lib/cmake \
+
+MIRROR_SYMBOLS := src/lib/qt5/genode/api/lib/symbols/libQt5Core \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Gui \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Network \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5PrintSupport \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Sql \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Svg \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Test \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Widgets \
+                  src/lib/qt5/genode/api/lib/symbols/libQt5Xml \
+                  src/lib/qt5/genode/api/lib/symbols/libqgenode
+
 
 content: $(MIRROR_FROM_PORT_DIR)
 
 $(MIRROR_FROM_PORT_DIR):
 	cp -r $(PORT_DIR)/$@ .
+
+content: $(MIRROR_SYMBOLS)
+
+$(MIRROR_SYMBOLS):
+	mkdir -p lib/symbols
+	cp $(PORT_DIR)/$@ lib/symbols
+
 
 content: LICENSE
 
