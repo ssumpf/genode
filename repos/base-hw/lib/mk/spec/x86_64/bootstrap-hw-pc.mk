8d2be087814559bfa7f094c35d3724b417b0994a
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2023-05-10 22:00:21 +0200

    hw: prevent alignment faults in bootstrap
    Either enable strict-alignment or disable FPU code generation by gcc
    regarding bootstrap code, which cannot handle alignment faults during
    initialization properly.
    
    Ref genodelabs/genode#4827

--- repos/base-hw/lib/mk/spec/x86_64/bootstrap-hw-pc.mk
+++ repos/base-hw/lib/mk/spec/x86_64/bootstrap-hw-pc.mk
@@ -6,4 +6,6 @@ SRC_S   += bootstrap/spec/x86_64/crt0_translation_table.s
 
 SRC_CC  += hw/spec/64bit/memory_map.cc
 
+CC_MARCH = -m64 -mcmodel=large -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx
+
 include $(call select_from_repositories,lib/mk/bootstrap-hw.inc)
