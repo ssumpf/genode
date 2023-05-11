51606ad03e155c2745ccbfa7cb83cc1f35da1a27
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2023-05-11 15:39:45 +0200

    fixup "hw: prevent alignment faults in bootstrap" (cc_march override)

--- repos/base-hw/lib/mk/spec/arm_v7/bootstrap-hw-virt_qemu_arm_v7a.mk
+++ repos/base-hw/lib/mk/spec/arm_v7/bootstrap-hw-virt_qemu_arm_v7a.mk
@@ -4,6 +4,6 @@ SRC_CC  += bootstrap/board/virt_qemu_arm_v7a/platform.cc
 SRC_CC  += bootstrap/spec/arm/cortex_a15_cpu.cc
 SRC_CC  += bootstrap/spec/arm/gicv2.cc
 
-CC_MARCH = -march=armv7ve+nofp -mtune=cortex-a15 -mfpu=vfpv3
-
 include $(call select_from_repositories,lib/mk/spec/arm_v7/bootstrap-hw.inc)
+
+CC_MARCH = -march=armv7ve+nofp -mtune=cortex-a15 -mfpu=vfpv3
