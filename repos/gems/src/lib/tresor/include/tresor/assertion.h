b0c673b6a106841ab13afb5894d8e6721de1d795
Martin Stein <martin.stein@genode-labs.com>
2023-06-09 13:50:34 +0200

    tresor: do not use on-disc-layouted structs
    * Removes all previous structs that represented an on-disc block layout
      and were therfore subject to a number of layout restrictions (packed,
      padding members, enum representations, etc.).
    * Adds a replacement struct without any layout restrictions for each of the
      removed structs. The new structs are named similar to the old structs.
    * Adds block encoding and decoding utilities for easily converting from the
      new structs to on-disc blocks and vice-versa (Block_scanner, Block_generator,
      T::decode_to_blk, T::encode_from_blk)
    * Adapts all affected places in the library to encode and decode proberly
      instead of simply casting pointers.
    * Thereby cleans up the hashing utilities to use typed-reference args instead
      of void pointers.
    * Re-enables run/tresor_tester and test-file_vault_vonfig_report for platforms
      rpi, imx53_qsb, imx53_qsb_tz, imx6q_sabrelite, imx7d_sabre.
    
    Ref #4819

---
+++ repos/gems/src/lib/tresor/include/tresor/assertion.h
@@ -0,0 +1,34 @@
+/*
+ * \brief  Assertion macros
+ * \author Martin Stein
+ * \date   2023-06-09
+ */
+
+/*
+ * Copyright (C) 2023 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _TRESOR__ASSERTION_H_
+#define _TRESOR__ASSERTION_H_
+
+/* base includes */
+#include <base/log.h>
+
+#define ASSERT(condition) \
+	do { \
+		if (!(condition)) { \
+			Genode::error(__FILE__, ":", __LINE__, ": ", " assertion \"", #condition, "\" failed "); \
+			for (;;); \
+		} \
+	} while (false)
+
+#define ASSERT_NEVER_REACHED \
+	do { \
+		Genode::error(__FILE__, ":", __LINE__, ": ", " should have never been reached"); \
+		for (;;); \
+	} while (false)
+
+#endif /* _TRESOR__ASSERTION_H_ */
