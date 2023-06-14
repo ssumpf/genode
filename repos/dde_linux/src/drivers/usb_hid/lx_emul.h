5ba46c4e9e4e442c86f20f5111a0985be80f32db
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-06-14 15:33:25 +0200

    dde_linux: update usb_hid driver to Linux 6.1.20
    The drivers uses the 'virt_linux' api and the current lx_kit
    implementation. It is a drop-in replacement for the Linux 4.16.3 based
    version.

---
+++ repos/dde_linux/src/drivers/usb_hid/lx_emul.h
@@ -0,0 +1,10 @@
+
+
+#include <lx_emul/debug.h>
+#include <linux/init.h>
+#include <linux/sched/debug.h>
+#include <linux/usb.h>
+//XXX: remove if possible
+struct input_dev_poller;
+struct input_event;
+struct usb_hub;
