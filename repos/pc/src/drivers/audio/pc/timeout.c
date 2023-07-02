504196f9b083272420ebd45e13d0fb944fb5fa1d
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-07-03 01:23:31 +0200

    pc/audio: wrap 'usleep_range_state' instead of 'usleep_range'
    'usleep_range' has become a static inline function in kernel 6.1 which
    we cannot wrap. The function calls 'usleep_range_state' instead, which
    we can wrap. We have to wrap this function because the SOF drivers calls
    'usleep_range' in the microsecond range. The original code schedules a
    timeout, which we don't support in sub-millisecond range, therefore,
    we implement it using 'udelay' for sound.

--- repos/pc/src/drivers/audio/pc/timeout.c
+++ repos/pc/src/drivers/audio/pc/timeout.c
@@ -1,7 +1,8 @@
 /*
  * \brief  For capture in 'hda_dsp_pcm_pointer' 'usleep_range' is called with
- *         20, 21 us. Since this calls 'schedule_hrtimeout_range' which sleeps
- *         for far too long, we implement 'usleep_range' with a 'udelay' here.
+ *         20, 21 us. Since this calls 'schedule_hrtimeout_range' through
+ *         'uslseep_range_state' which sleeps for far too long, we implement
+ *         'usleep_range_state' with a 'udelay' here.
  * \author Sebastian Sumpf
  * \date   2022-05-28
  */
@@ -16,7 +17,8 @@
 #include <lx_emul/time.h>
 #include <asm-generic/delay.h>
 
-void __wrap_usleep_range(unsigned long min, unsigned long max)
+void __wrap_usleep_range_state(unsigned long min, unsigned long max,
+                               unsigned int state)
 {
 	udelay(min);
 }
