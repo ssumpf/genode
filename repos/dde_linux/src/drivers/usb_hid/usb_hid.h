bb7483bccdc0de5f36fc5a5577066077a35f70ca
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-07-05 18:54:44 +0200

    usb_hid: use lx_emul for Linux device probing
    Use 'lx_emul/virt/usb_client.c' to probe the device for given USB client
    handle.

--- repos/dde_linux/src/drivers/usb_hid/usb_hid.h
+++ repos/dde_linux/src/drivers/usb_hid/usb_hid.h
@@ -12,14 +12,11 @@
  */
 
 #include <genode_c_api/usb_client.h>
+#include <lx_emul/usb_client.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
-void *lx_emul_usb_client_register_device(genode_usb_client_handle_t handle, char const *label);
-void  lx_emul_usb_client_unregister_device(genode_usb_client_handle_t handle, void *data);
 
 struct task_struct;
 
