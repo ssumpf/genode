d1f8bb4d7560f7c0848db1d9a1203aa258f421a7
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-07-07 21:06:41 +0200

    virt/lx_emul: cleanup urb.c

--- repos/dde_linux/src/lib/lx_emul/virt/shadow/drivers/usb/core/urb.c
+++ repos/dde_linux/src/lib/lx_emul/virt/shadow/drivers/usb/core/urb.c
@@ -311,9 +311,8 @@ int usb_set_configuration(struct usb_device *dev, int configuration)
 
 	for (i = 0; i < nintf; ++i) {
 		struct usb_interface *intf = cp->interface[i];
-		printk("%s:%d ADD\n", __func__, __LINE__);
+
 		ret = device_add(&intf->dev);
-		printk("%s:%d ADD DONE\n", __func__, __LINE__);
 		if (ret != 0) {
 			printk("error: device_add(%s) --> %d\n", dev_name(&intf->dev), ret);
 			continue;
@@ -329,7 +328,7 @@ void usb_enable_endpoint(struct usb_device *dev, struct usb_host_endpoint *ep, b
 	int epnum = usb_endpoint_num(&ep->desc);
 	int is_out = usb_endpoint_dir_out(&ep->desc);
 	int is_control = usb_endpoint_xfer_control(&ep->desc);
-	printk("%s:%d ADD EP: num %d out %d control: %d\n", __func__, __LINE__, epnum, is_out, is_control);
+
 	if (is_out || is_control)
 		dev->ep_out[epnum] = ep;
 	if (!is_out || is_control)
@@ -411,7 +410,7 @@ int usb_set_interface(struct usb_device *udev, int ifnum, int alternate)
 	usb_enable_interface(udev, iface, true);
 
 	genode_usb_client_request_finish(handle, &packet);
-	printk("%s:%d ret: %d\n", __func__, __LINE__, 0);
+
 err_request:
 	kfree(urb);
 
