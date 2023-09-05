c95a35f48a670cb517516a189ac1dc936b777605
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-09-05 18:12:09 +0200

    lxip: startup complete

--- repos/dde_linux/src/lib/lxip/lx_emul.c
+++ repos/dde_linux/src/lib/lxip/lx_emul.c
@@ -67,12 +67,52 @@ void *alloc_large_system_hash(const char *tablename,
 }
 
 
-#include <linux/net.h>
+#include <linux/syscore_ops.h>
+
+void register_syscore_ops(struct syscore_ops * ops)
+{
+	lx_emul_trace(__func__);
+}
+
+
+#include <linux/rcutree.h>
+
+/*
+ * taken from src/lib/wifi/lx_emul.c
+ */
+void kvfree_call_rcu(struct rcu_head * head, rcu_callback_t func)
+{
+	void *ptr = (void *) head - (unsigned long) func;
+	kvfree(ptr);
+}
+
+
+/* mm/page_alloc. */
+
+/**
+ * nr_free_buffer_pages - count number of pages beyond high watermark
+ *
+ * nr_free_buffer_pages() counts the number of pages which are beyond the high
+ * watermark within ZONE_DMA and ZONE_NORMAL.
+ *
+ * used in 'tcp_mem_init'
+ *
+ * limit = max(nr_free_buffer_pages()/16, 128)
+ * -> set to max
+ */
+unsigned long nr_free_buffer_pages(void)
+{
+	return 2048;
+}
 
 
 /****************************
  ** Linux socket functions **
  ****************************/
+
+#include <linux/net.h>
+#include <linux/skbuff.h>
+
 
 static const struct net_proto_family *net_families[NPROTO];
 
@@ -121,5 +161,55 @@ int sock_create_lite(int family, int type, int protocol, struct socket **res)
 	*res = sock;
 	return 0;
 }
+
+
+int sock_create_kern(struct net *net, int family, int type, int proto,
+                     struct socket **res)
+{
+	struct socket *sock;
+	const struct net_proto_family *pf;
+	int err;
+
+	if (family < 0 || family > NPROTO)
+		return -EAFNOSUPPORT;
+
+	if (type < 0 || type > SOCK_MAX)
+		return -EINVAL;
+
+	pf = net_families[family];
+
+	if (!pf) {
+		printk("No protocol found for family %d\n", family);
+		return -ENOPROTOOPT;
+	}
+
+	sock = sock_alloc();
+	if (!sock) {
+		printk("Could not allocate socket\n");
+		return -ENFILE;
+	}
+
+	sock->type = type;
+
+	err = pf->create(net, sock, proto, 1);
+	if (err) {
+		kfree(sock);
+		return err;
+	}
+
+	*res = sock;
+
+	return 0;
+}
+
+
+static int sock_init(void)
+{
+	skb_init();
+	return 0;
+}
+
+
+core_initcall(sock_init);
 
 
