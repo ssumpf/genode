df8b362e731fcae5d851ee94ec26aa69567221e4
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-09-18 11:42:39 +0200

    lxip: listen

--- repos/dde_linux/src/lib/lxip/include/genode_c_api/socket_types.h
+++ repos/dde_linux/src/lib/lxip/include/genode_c_api/socket_types.h
@@ -3,8 +3,8 @@ enum {
 	/* socket domain */
 	AF_INET = 2,
 	/* socket type */
-	SOCK_DGRAM  = 1,
-	SOCK_STREAM = 2,
+	SOCK_STREAM = 1,
+	SOCK_DGRAM  = 2,
 	/* protocol */
 	IPPROTO_TCP = 6,
 	IPPROTO_UDP = 17,
