cc2ef674a408c802798ecf631b7e7ecd4c0233ba
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-12-12 15:39:12 +0100

    libports: test libc no accept
    Add small test that opens a socket, listens, but does not accept. This
    is used for connect timeout testing.

---
+++ repos/libports/recipes/src/test-libc_no_accept/content.mk
@@ -0,0 +1,3 @@
+SRC_DIR = src/test/libc_no_accept
+
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
---
+++ repos/libports/src/test/libc_no_accept/target.mk
@@ -0,0 +1,5 @@
+TARGET = test-libc_no_accept
+SRC_CC = main.cc
+LIBS   = posix
+
+CC_CXX_WARN_STRICT =
