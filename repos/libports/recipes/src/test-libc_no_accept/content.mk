39f6ed9288467d617abe2e04694ab697476f1fe8
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-12-14 18:07:24 +0100

    Revert "libports: test libc no accept"
    This reverts commit cc2ef674a408c802798ecf631b7e7ecd4c0233ba.

--- repos/libports/recipes/src/test-libc_no_accept/content.mk
+++
@@ -1,3 +0,0 @@
-SRC_DIR = src/test/libc_no_accept
-
-include $(GENODE_DIR)/repos/base/recipes/src/content.inc
--- repos/libports/src/test/libc_no_accept/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET = test-libc_no_accept
-SRC_CC = main.cc
-LIBS   = posix
-
-CC_CXX_WARN_STRICT =
