5a193f0df8ade81a554358e8b4de2d99ab86c917
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2024-02-29 18:10:38 +0100

    mesa-24: compile softpipe

--- repos/libports/lib/mk/spec/arm_v8/etnaviv.mk
+++ repos/libports/lib/mk/spec/arm_v8/etnaviv.mk
@@ -12,52 +12,50 @@ INC_DIR += $(MESA_SRC_DIR)/src/compiler/nir \
 
 REP_INC_DIR += include/drm-uapi
 
-
-SRC_C = etnaviv/drm/etnaviv_bo.c \
-        etnaviv/drm/etnaviv_bo_cache.c \
-        etnaviv/drm/etnaviv_cmd_stream.c \
-        etnaviv/drm/etnaviv_device.c \
-        etnaviv/drm/etnaviv_gpu.c \
-        etnaviv/drm/etnaviv_perfmon.c \
-        etnaviv/drm/etnaviv_pipe.c \
-        gallium/drivers/etnaviv/etnaviv_asm.c \
-        gallium/drivers/etnaviv/etnaviv_blend.c \
-        gallium/drivers/etnaviv/etnaviv_blt.c \
-        gallium/drivers/etnaviv/etnaviv_clear_blit.c \
-        gallium/drivers/etnaviv/etnaviv_compiler.c \
-        gallium/drivers/etnaviv/etnaviv_compiler_nir.c \
-        gallium/drivers/etnaviv/etnaviv_compiler_nir_emit.c \
-        gallium/drivers/etnaviv/etnaviv_compiler_nir_liveness.c \
-        gallium/drivers/etnaviv/etnaviv_compiler_nir_ra.c \
-        gallium/drivers/etnaviv/etnaviv_compiler_tgsi.c \
-        gallium/drivers/etnaviv/etnaviv_context.c \
-        gallium/drivers/etnaviv/etnaviv_disasm.c \
-        gallium/drivers/etnaviv/etnaviv_disk_cache.c \
-        gallium/drivers/etnaviv/etnaviv_emit.c \
-        gallium/drivers/etnaviv/etnaviv_etc2.c \
-        gallium/drivers/etnaviv/etnaviv_fence.c \
-        gallium/drivers/etnaviv/etnaviv_format.c \
-        gallium/drivers/etnaviv/etnaviv_nir.c \
-        gallium/drivers/etnaviv/etnaviv_perfmon.c \
-        gallium/drivers/etnaviv/etnaviv_query_acc.c \
-        gallium/drivers/etnaviv/etnaviv_query_acc_occlusion.c \
-        gallium/drivers/etnaviv/etnaviv_query_acc_perfmon.c \
-        gallium/drivers/etnaviv/etnaviv_query.c \
-        gallium/drivers/etnaviv/etnaviv_query_sw.c \
-        gallium/drivers/etnaviv/etnaviv_rasterizer.c \
-        gallium/drivers/etnaviv/etnaviv_resource.c \
-        gallium/drivers/etnaviv/etnaviv_rs.c \
-        gallium/drivers/etnaviv/etnaviv_screen.c \
-        gallium/drivers/etnaviv/etnaviv_shader.c \
-        gallium/drivers/etnaviv/etnaviv_state.c \
-        gallium/drivers/etnaviv/etnaviv_surface.c \
-        gallium/drivers/etnaviv/etnaviv_texture.c \
-        gallium/drivers/etnaviv/etnaviv_texture_desc.c \
-        gallium/drivers/etnaviv/etnaviv_texture_state.c \
-        gallium/drivers/etnaviv/etnaviv_tiling.c \
-        gallium/drivers/etnaviv/etnaviv_transfer.c \
-        gallium/drivers/etnaviv/etnaviv_uniforms.c \
-        gallium/winsys/etnaviv/drm/etnaviv_drm_winsys.c \
-        gallium/drivers/etnaviv/etnaviv_zsa.c
+SRC_C = src/etnaviv/drm/etnaviv_bo.c \
+        src/etnaviv/drm/etnaviv_bo_cache.c \
+        src/etnaviv/drm/etnaviv_cmd_stream.c \
+        src/etnaviv/drm/etnaviv_device.c \
+        src/etnaviv/drm/etnaviv_gpu.c \
+        src/etnaviv/drm/etnaviv_perfmon.c \
+        src/etnaviv/drm/etnaviv_pipe.c \
+        src/gallium/drivers/etnaviv/etnaviv_asm.c \
+        src/gallium/drivers/etnaviv/etnaviv_blend.c \
+        src/gallium/drivers/etnaviv/etnaviv_blt.c \
+        src/gallium/drivers/etnaviv/etnaviv_clear_blit.c \
+        src/gallium/drivers/etnaviv/etnaviv_compiler.c \
+        src/gallium/drivers/etnaviv/etnaviv_compiler_nir.c \
+        src/gallium/drivers/etnaviv/etnaviv_compiler_nir_emit.c \
+        src/gallium/drivers/etnaviv/etnaviv_compiler_nir_liveness.c \
+        src/gallium/drivers/etnaviv/etnaviv_compiler_nir_ra.c \
+        src/gallium/drivers/etnaviv/etnaviv_context.c \
+        src/gallium/drivers/etnaviv/etnaviv_disasm.c \
+        src/gallium/drivers/etnaviv/etnaviv_disk_cache.c \
+        src/gallium/drivers/etnaviv/etnaviv_emit.c \
+        src/gallium/drivers/etnaviv/etnaviv_etc2.c \
+        src/gallium/drivers/etnaviv/etnaviv_fence.c \
+        src/gallium/drivers/etnaviv/etnaviv_format.c \
+        src/gallium/drivers/etnaviv/etnaviv_nir.c \
+        src/gallium/drivers/etnaviv/etnaviv_perfmon.c \
+        src/gallium/drivers/etnaviv/etnaviv_query.c \
+        src/gallium/drivers/etnaviv/etnaviv_query_acc.c \
+        src/gallium/drivers/etnaviv/etnaviv_query_acc_occlusion.c \
+        src/gallium/drivers/etnaviv/etnaviv_query_acc_perfmon.c \
+        src/gallium/drivers/etnaviv/etnaviv_query_sw.c \
+        src/gallium/drivers/etnaviv/etnaviv_rasterizer.c \
+        src/gallium/drivers/etnaviv/etnaviv_resource.c \
+        src/gallium/drivers/etnaviv/etnaviv_rs.c \
+        src/gallium/drivers/etnaviv/etnaviv_screen.c \
+        src/gallium/drivers/etnaviv/etnaviv_shader.c \
+        src/gallium/drivers/etnaviv/etnaviv_state.c \
+        src/gallium/drivers/etnaviv/etnaviv_surface.c \
+        src/gallium/drivers/etnaviv/etnaviv_texture.c \
+        src/gallium/drivers/etnaviv/etnaviv_texture_desc.c \
+        src/gallium/drivers/etnaviv/etnaviv_texture_state.c \
+        src/gallium/drivers/etnaviv/etnaviv_tiling.c \
+        src/gallium/drivers/etnaviv/etnaviv_transfer.c \
+        src/gallium/drivers/etnaviv/etnaviv_uniforms.c \
+        src/gallium/drivers/etnaviv/etnaviv_zsa.c
+        src/gallium/winsys/etnaviv/drm/etnaviv_drm_winsys.c \
 
 vpath %.c $(MESA_SRC_DIR)/src
