43e42e4e9aa7d3001d65a3c29664c98933575570
Josef Söntgen <josef.soentgen@genode-labs.com>
2024-11-11 16:22:50 +0100

    TMP DRIVERS modular_sculpt: add Ubuntu installer preset

--- repos/modular_sculpt/recipes/raw/vbox_files/content.mk
+++ repos/modular_sculpt/recipes/raw/vbox_files/content.mk
@@ -2,7 +2,7 @@ content: vbox_files.tar
 
 TAR := tar --owner=0 --group=0 --numeric-owner --mode='go=' --mtime='1970-01-01 00:00+00'
 
-FILES := ubuntu-22.04-raw \
-		 windows-10-raw
+FILES := ubuntu-22.04-raw ubuntu-22.04-installer-raw \
+         windows-10-raw
 vbox_files.tar:
 	$(TAR) -cf $@ -C $(REP_DIR)/recipes/raw/vbox_files $(FILES)
