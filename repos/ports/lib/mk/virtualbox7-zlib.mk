d074d3293a2fd2c0585435546df4916dfe7720c1
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2025-06-18 19:33:34 +0200

    vbox7: lib/virtualbox7-zlib

---
+++ repos/ports/lib/mk/virtualbox7-zlib.mk
@@ -0,0 +1,11 @@
+include $(REP_DIR)/lib/mk/virtualbox7-common.inc
+
+ZLIB_DIR = $(VIRTUALBOX_DIR)/src/libs/zlib-1.2.13
+INC_DIR += $(ZLIB_DIR)
+SRC_C    = $(notdir $(wildcard $(ZLIB_DIR)/*.c))
+
+CC_OPT += -DHAVE_UNISTD_H=1
+
+vpath % $(ZLIB_DIR)
+
+CC_CXX_WARN_STRICT =
