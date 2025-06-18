1d74ab7799ffa58e91b19dba11e16088afb46300
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2025-06-18 20:52:46 +0200

    fixup: 'lib/virtualbox7-dis'

--- repos/ports/lib/mk/spec/x86_64/virtualbox7-dis.mk
+++ repos/ports/lib/mk/spec/x86_64/virtualbox7-dis.mk
@@ -10,6 +10,13 @@ SRC_CC += VMM/VMMR3/DBGFReg.cpp
 
 SRC_CC += $(addprefix Disassembler/, $(notdir $(wildcard $(VBOX_DIR)/Disassembler/*.cpp)))
 
+FILTERED_OUT_SRC_CC += Disassembler/DisasmFormatArmV8.cpp
+FILTERED_OUT_SRC_CC += Disassembler/DisasmCore-armv8.cpp
+FILTERED_OUT_SRC_CC += Disassembler/DisasmReg.cpp
+FILTERED_OUT_SRC_CC += Disassembler/DisasmTables-armv8-a64.cpp
+
+SRC_CC := $(filter-out $(FILTERED_OUT_SRC_CC), $(SRC_CC))
+
 INC_DIR += $(VBOX_DIR)/VMM/include
 
 CC_OPT += -DVBOX_IN_VMM
