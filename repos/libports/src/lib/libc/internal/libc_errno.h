454195f357b11d6d2d6ac32edb203ca3f518c2a1
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2025-12-02 13:37:08 +0100

    libc/socket_fs: use error file
    * Retrieve Genode::Errno from 'error' file and translate it libc's errno
    
    * Adjust lwip to return the same results as lxip
    
    * Adjust tests to new errno's
    
    issue #5768

---
+++ repos/libports/src/lib/libc/internal/libc_errno.h
@@ -0,0 +1,23 @@
+/*
+ * \brief  Convert Genode errno to libc errno
+ * \author Sebastian Sumpf
+ * \date   2025-12-03
+ */
+
+/*
+ * Copyright (C) 2025 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _LIBC__INTERNAL__LBIC_ERRNO_H_
+#define _LIBC__INTERNAL__LBIC_ERRNO_H_
+
+#include <base/node.h>
+
+namespace Libc {
+	int libc_errno(int genode_errno);
+}
+
+#endif /* _LIBC__INTERNAL__LBIC_ERRNO_H_ */
