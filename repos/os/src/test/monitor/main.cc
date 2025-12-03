e1aacfaa020cb372a5868b2631edbca266bf3c42
Norman Feske <norman.feske@genode-labs.com>
2025-12-03 13:10:22 +0100

    fixup "util/string.h: replace memset by bzero" (ip_raw,monitor)

--- repos/os/src/test/monitor/main.cc
+++ repos/os/src/test/monitor/main.cc
@@ -124,7 +124,7 @@ struct Test::Main
 		Attached_ram_dataspace large_ram_ds { _env.ram(), _env.rm(), 8*1024*1024 };
 
 		for (size_t i = 0; i < large_ram_ds.size(); i++)
-			large_ram_ds_ds.local_addr<char>()[i] = 1;
+			large_ram_ds.local_addr<char>()[i] = 1;
 
 		/*
 		 * Dimensioning of the buffer for one round trip:
