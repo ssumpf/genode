dd85e04d03c70591303d86af0d0aff64eac88126
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2026-01-12 19:08:06 +0100

    vbox7: improve sync during monitor change

--- repos/ports/src/virtualbox7/main.cc
+++ repos/ports/src/virtualbox7/main.cc
@@ -484,7 +484,7 @@ Main::Monitor::Fb::Fb(Env &env, Gui::Connection &gui,
                       ComPtr<IDisplay> const &display, unsigned id,
                       Gui::Top_level_view &view)
 :
-	id(id), display(display), fb(new Genodefb(env, gui, display, id, view))
+	id(id), display(display), fb(new Genodefb(env, &gui, display, id, &view))
 {
 	display->AttachFramebuffer(id, fb, guid.asOutParam());
 }
@@ -541,6 +541,11 @@ Main::Monitor::Monitor(Registry<Monitor> &registry, Env &env,
 
 Main::Monitor::~Monitor()
 {
+	synchronize([&]() {
+		if (fb.constructed() && !fb->fb.isNull())
+			fb->fb->invalidate_gui();
+	});
+
 	display->SetVideoModeHint(id, false, false, 0, 0, 0, 0, 0, true);
 }
 
