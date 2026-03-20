4128505236629a183f2f865167565de04a922ba2
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2026-03-20 12:58:29 +0100

    Revert "event_filter: allow 'include' inside 'merge' nodes"
    Issue #5830
    
    This reverts commit 66b2e7ffc03e247e33215eec33731809e9f39dc2.

--- repos/os/src/server/event_filter/merge_source.h
+++ repos/os/src/server/event_filter/merge_source.h
@@ -15,7 +15,6 @@
 #define _EVENT_FILTER__MERGE_SOURCE_H_
 
 /* local includes */
-#include <include_accessor.h>
 #include <source.h>
 
 namespace Event_filter { class Merge_source; }
@@ -27,54 +26,17 @@ class Event_filter::Merge_source : public Source
 
 		Owner _owner;
 
-		Include_accessor &_include_accessor;
-
-		void _apply_config(Node const &config, unsigned const max_recursion = 4)
-		{
-			config.for_each_sub_node([&] (Node const &node) {
-				_apply_sub_node(node, max_recursion); });
-		}
-
-		void _apply_sub_node(Node const &node, unsigned const max_recursion)
-		{
-			if (max_recursion == 0) {
-				warning("too deeply nested includes");
-				throw Invalid_config();
-			}
-
-			/*
-			 * Handle includes
-			 */
-			if (node.type() == "include") {
-				try {
-					Include_accessor::Name const rom =
-						node.attribute_value("rom", Include_accessor::Name());
-
-					_include_accessor.apply_include(rom, name(), [&] (Node const &inc) {
-						_apply_config(inc, max_recursion - 1); });
-					return;
-				}
-				catch (Include_accessor::Include_unavailable) {
-					throw Invalid_config(); }
-			}
-
-			/*
-			 * Handle input nodes
-			 */
-			if (input_node(node))
-				_owner._factory.create_source(_owner, node);
-		}
-
 	public:
 
 		static char const *name() { return "merge"; }
 
-		Merge_source(Owner &owner, Node const &config, Source::Factory &factory,
-		             Include_accessor &include_accessor)
+		Merge_source(Owner &owner, Node const &config, Source::Factory &factory)
 		:
-			Source(owner), _owner(factory), _include_accessor(include_accessor)
+			Source(owner), _owner(factory)
 		{
-			_apply_config(config);
+			config.for_each_sub_node([&] (Node const &node) {
+				if (input_node(node))
+					factory.create_source(_owner, node); });
 		}
 
 		void generate(Sink &destination) override
