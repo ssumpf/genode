577ce0725b34995b49654ad6569b4ec710420f99
Norman Feske <norman.feske@genode-labs.com>
2026-03-21 21:13:50 +0100

    sculpt: move log infrastructure to sculpt option
    This patch moves the infrastructure for logging and reporting from the
    static system to the runtime level. The new 'report' subsystem features
    the following services:
    
    - Report service to which clients can submit their state in a
      fire-and-forget fashion.
    
    - Read-only file system that exports the client-submitted reports as
      as directory tree organized according to the labels of the reporters.
      Since the report service is now hosted in the runtime, the runtime/
      at the root of the traditional report_fs is no more.
    
    - Log service that writes messages to the 'log' file at the root of
      the report file system.
    
    - ROM service for accessing individual reports as ROM modules.
    
    Fixes #5839

--- repos/gems/src/app/phone_manager/main.cc
+++ repos/gems/src/app/phone_manager/main.cc
@@ -421,7 +421,7 @@ struct Sculpt::Main : Input_event_handler,
 	 ************/
 
 	Rom_handler<Main> _update_state_rom {
-		_env, "report -> runtime/update/state", *this, &Main::_handle_update_state };
+		_env, "report -> update/state", *this, &Main::_handle_update_state };
 
 	Managed_config<Main> _install {
 		_env, _heap, "install", "install", *this, &Main::_handle_install };
@@ -522,7 +522,7 @@ struct Sculpt::Main : Input_event_handler,
 	}
 
 	Rom_handler<Main> _depot_query_blueprint_rom {
-		_env, "report -> runtime/depot_query/blueprint", *this, &Main::_handle_depot_query_blueprint };
+		_env, "report -> depot_query/blueprint", *this, &Main::_handle_depot_query_blueprint };
 
 	void _handle_depot_query_blueprint(Node const &blueprint)
 	{
@@ -548,7 +548,7 @@ struct Sculpt::Main : Input_event_handler,
 	 ******************/
 
 	Rom_handler<Main> _index_rom {
-		_env, "report -> runtime/depot_query/index", *this, &Main::_handle_index };
+		_env, "report -> depot_query/index", *this, &Main::_handle_index };
 
 	void _handle_index(Node const &)
 	{
@@ -573,7 +573,7 @@ struct Sculpt::Main : Input_event_handler,
 	 ************/
 
 	Rom_handler<Main> _scan_rom {
-		_env, "report -> runtime/depot_query/scan", *this, &Main::_handle_scan };
+		_env, "report -> depot_query/scan", *this, &Main::_handle_scan };
 
 	void _handle_scan(Node const &)
 	{
@@ -583,7 +583,7 @@ struct Sculpt::Main : Input_event_handler,
 	}
 
 	Rom_handler<Main> _image_index_rom {
-		_env, "report -> runtime/depot_query/image_index", *this, &Main::_handle_image_index };
+		_env, "report -> depot_query/image_index", *this, &Main::_handle_image_index };
 
 	void _handle_image_index(Node const &) { _generate_dialog(); }
 
@@ -591,7 +591,7 @@ struct Sculpt::Main : Input_event_handler,
 	Presets _presets { _heap };
 
 	Rom_handler<Main> _config_listing_rom {
-		_env, "report -> /runtime/config_query/listing", *this,
+		_env, "report -> config_query/listing", *this,
 		&Main::_handle_config_listing };
 
 	void _handle_config_listing(Node const &listing)
@@ -984,7 +984,7 @@ struct Sculpt::Main : Input_event_handler,
 	}
 
 	Rom_handler<Main> _runtime_state_rom {
-		_env, "report -> runtime/state", *this, &Main::_handle_runtime_state };
+		_env, "report -> state", *this, &Main::_handle_runtime_state };
 
 	void _handle_runtime_state(Node const &);
 
