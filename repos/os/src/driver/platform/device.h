062a07ee1025b64e966546dda0dc84944a343d02
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2026-03-30 16:26:20 +0200

    !fixup: platform: re-enable kernel-controlled IOMMU on AMD
    Issue #5823

--- repos/os/src/driver/platform/device.h
+++ repos/os/src/driver/platform/device.h
@@ -547,19 +547,19 @@ class Driver::Device_model : public Device_owner
 				if (sirq.number() == number) fn(sirq); });
 		}
 
-		void with_io_mmu(Device::Name const &name, auto const &fn)
+		void with_io_mmu(Device::Name const &iommu_name, auto const &fn)
 		{
-			if (_kernel_io_mmu.constructed())
-				fn(*_kernel_io_mmu);
-			else
-				_io_mmus.for_each([&] (auto &io_mmu) {
-					if (io_mmu.name() == name) fn(io_mmu); });
+			Device::Name const name = _kernel_io_mmu.constructed()
+				? _kernel_io_mmu->name() : iommu_name;
+
+			_io_mmus.for_each([&] (auto &io_mmu) {
+				if (io_mmu.name() == name) fn(io_mmu); });
 		}
 
 		void with_io_mmu(Device const &dev, auto const &fn)
 		{
 			if (_kernel_io_mmu.constructed())
-				with_io_mmu(_kernel_io_mmu->name, fn);
+				with_io_mmu(_kernel_io_mmu->name(), fn);
 			else
 				dev.with_io_mmu([&] (auto const &im) {
 					with_io_mmu(im.name, fn); });
