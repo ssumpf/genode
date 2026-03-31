3018deb9033a1b7f7080bab0a0f7f51dbb3d5eb3
Christian Helmuth <christian.helmuth@genode-labs.com>
2026-03-31 08:07:15 +0200

    Revert "pc_i2c_hid: make driver configurable"
    This reverts commit f1ef9176f6dfe4b9492d6aa9a6c3185e1abaaf34.

--- repos/pc/src/driver/i2c_hid/pc/generated_dummies.c
+++ repos/pc/src/driver/i2c_hid/pc/generated_dummies.c
@@ -1,7 +1,7 @@
 /*
  * \brief  Dummy definitions of Linux Kernel functions
  * \author Automatically generated file - do no edit
- * \date   2026-03-24
+ * \date   2025-09-26
  */
 
 #include <lx_emul.h>
@@ -63,6 +63,14 @@ void __printk_deferred_enter(void)
 #include <linux/printk.h>
 
 void __printk_deferred_exit(void)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+#include <linux/mm.h>
+
+void __show_mem(unsigned int filter,nodemask_t * nodemask,int max_zone_idx)
 {
 	lx_emul_trace_and_stop(__func__);
 }
@@ -313,14 +321,6 @@ void drm_panel_remove_follower(struct drm_panel_follower * follower)
 }
 
 
-#include <linux/mmdebug.h>
-
-void dump_page(const struct page * page,const char * reason)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
 #include <linux/printk.h>
 
 asmlinkage __visible void dump_stack(void)
@@ -377,14 +377,6 @@ void fput(struct file * file)
 }
 
 
-#include <linux/fs.h>
-
-struct file * get_file_active(struct file ** f)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
 #include <linux/file.h>
 
 int get_unused_fd_flags(unsigned flags)
@@ -414,13 +406,6 @@ struct pseudo_fs_context * init_pseudo(struct fs_context * fc,unsigned long magi
 bool initcall_debug;
 
 
-extern int input_bits_to_string(char * buf,int buf_size,unsigned long bits,bool skip_empty);
-int input_bits_to_string(char * buf,int buf_size,unsigned long bits,bool skip_empty)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
 #include <linux/sched.h>
 
 void __sched io_schedule(void)
@@ -448,14 +433,6 @@ int io_schedule_prepare(void)
 #include <linux/sched.h>
 
 long __sched io_schedule_timeout(long timeout)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
-#include <linux/irq_work.h>
-
-bool irq_work_queue(struct irq_work * work)
 {
 	lx_emul_trace_and_stop(__func__);
 }
@@ -511,9 +488,17 @@ int kobject_synth_uevent(struct kobject * kobj,const char * buf,size_t count)
 unsigned long loops_per_jiffy;
 
 
-#include <linux/mman.h>
+#include <linux/preempt.h>
 
-void mm_compute_batch(int overcommit_policy)
+void migrate_disable(void)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+#include <linux/preempt.h>
+
+void migrate_enable(void)
 {
 	lx_emul_trace_and_stop(__func__);
 }
@@ -575,43 +560,14 @@ int of_clk_hw_register(struct device_node * node,struct clk_hw * hw)
 }
 
 
-#include <linux/printk.h>
-
-int oops_in_progress;	/* If set, an oops, panic(), BUG() or die() is in progress */
-
-
 #include <linux/reboot.h>
 
 enum reboot_mode panic_reboot_mode;
 
 
-#include <linux/pci.h>
-
-const struct cpumask * pci_irq_get_affinity(struct pci_dev * dev,int nr)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
-#include <linux/percpu_counter.h>
-
-void percpu_counter_sync(struct percpu_counter * fbc)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
 #include <linux/printk.h>
 
 void printk_legacy_allow_panic_sync(void)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
-#include <linux/sysctl.h>
-
-int proc_dointvec(const struct ctl_table * table,int write,void * buffer,size_t * lenp,loff_t * ppos)
 {
 	lx_emul_trace_and_stop(__func__);
 }
@@ -628,14 +584,6 @@ int proc_dointvec_minmax(const struct ctl_table * table,int write,void * buffer,
 #include <linux/sysctl.h>
 
 int proc_douintvec(const struct ctl_table * table,int write,void * buffer,size_t * lenp,loff_t * ppos)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
-#include <linux/sysctl.h>
-
-int proc_doulongvec_minmax(const struct ctl_table * table,int write,void * buffer,size_t * lenp,loff_t * ppos)
 {
 	lx_emul_trace_and_stop(__func__);
 }
@@ -765,6 +713,14 @@ void set_rq_online(struct rq * rq)
 #include <linux/smp.h>
 
 unsigned int setup_max_cpus;
+
+
+#include <linux/sched/debug.h>
+
+void show_state_filter(unsigned int state_filter)
+{
+	lx_emul_trace_and_stop(__func__);
+}
 
 
 #include <linux/smp.h>
